f95d01bff604630ade107f51cf6f5c71699081d7
Alex Zvoleff <azvoleff@conservation.org>
2013-09-16 17:36:39 -0400

    Code simplify_ZOI function.

--- R/simplify_ZOI.R
+++ R/simplify_ZOI.R
@@ -7,7 +7,40 @@
 #' shapefiles.
 #' @examples
 #' # TODO: Write an example.
-simplify_ZOI <- function(shapefile) {
-    # TODO: Write this function
-    require(shapefiles)
+#' require(rgdal)
+#' require(rgeos)
+#' require(sp)
+ZOI_poly <- readOGR('H:/Data/TEAM/VB/Vectors', 'VB_ZOI_GEO')
+simplify_ZOI(ZOI_poly, 10, 3)
+# TODO: Might need to load 'shapefiles' package to get extent function
+
+simplify_ZOI <- function(ZOI_poly, max_vertices, maxit=100) {
+    n_parts <- sapply(ZOI_poly@polygons, function(x) length(x))
+    if (length(n_parts) > 1)
+        stop('Error: ZOI shapefile contains more than one polygon')
+    if (n_parts > 1)
+        stop('Error: ZOI polygon is a multipart polygon')
+
+    # Set the initial tolerance as the extent / 100
+    ext <- extent(ZOI_poly)
+    diag_seg_length <- sqrt((ext@xmax - ext@xmin)**2 +
+                            (ext@ymax - ext@ymin)**2)
+    tolerance <- diag_seg_length / 100
+
+    # Iterate, increasing tolerance, until polygon has less than maxpts 
+    # vertices.
+    n_verts <- sapply(ZOI_poly@polygons, function(y) nrow(y@Polygons[[1]]@coords))
+    n <- 0
+    while ((n_verts > max_vertices) & (n < maxit)) {
+        ZOI_poly <- gSimplify(ZOI_poly, tol=tolerance)
+        n_verts <- sapply(ZOI_poly@polygons, function(y) nrow(y@Polygons[[1]]@coords))
+        tolerance <- tolerance * 1.5
+        print(tolerance)
+        print(n_verts)
+        n <- n + 1
+    }
+    if (n == maxit)
+        warning(paste('reached maximum iterations (', maxit, ')', sep=''))
+    return (ZOI_poly)
 }
+
