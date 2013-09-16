a85dd1c59444d96b708a212937677be8ce3f8892
Alex Zvoleff <azvoleff@conservation.org>
2013-09-16 15:42:06 -0400

    Add script to simplify ZOI polygons.

---
+++ R/simplify_ZOI.R
@@ -0,0 +1,13 @@
+#' Simplify a TEAM site ZOI polygon for use with EarthExplorer
+#'
+#' @export
+#' @param shapefile a shapefile of a TEAM site ZOI boundary polygon
+#' @return The TEAM site ZOI boundary polygon simplified for use with online 
+#' data portals that limit the number of vertices allowed in uploaded AOI 
+#' shapefiles.
+#' @examples
+#' # TODO: Write an example.
+simplify_ZOI <- function(shapefile) {
+    # TODO: Write this function
+    require(shapefiles)
+}
