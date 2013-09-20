283ee377468716907b86a28cd7056343fdb2f604
Alex Zvoleff <azvoleff@conservation.org>
2013-09-20 17:07:22 -0400

    Add CVAPS code.

---
+++ R/CVAPS.R
@@ -0,0 +1,44 @@
+#' Change Vector Analysis in Posterior Probability Space
+#'
+#' @export
+#' @param svm_t0 Support Vector Machine for time 0
+#' @param x_t0 Raster object with predictor layers for time 0
+#' @param svm_t1 Support Vector Machine for time 1
+#' @param x_t1 Raster object with predictor layers for time 1
+#' @references Chen, J., X. Chen, X. Cui, and J. Chen. 2011. Change vector 
+#' analysis in posterior probability space: a new method for land cover change 
+#' detection. IEEE Geoscience and Remote Sensing Letters 8:317–321.
+CVAPS <- function(svm_t0, x_t0, svm_t1, x_t1) {
+    # Check that x_t0 and x_t1 are matched images (identical extents and 
+    # identical projections)
+    
+    # Check that svm_t0 and svm_t1 have the same classes
+    
+    # Get number of classes from coefs matrix
+    n_classes <- svm_t0@coefs
+
+    pred_t0 <- predict(svm_t0, newdata=x_t0, probability=TRUE)
+    pred_t1 <- predict(svm_t1, newdata=x_t1, probability=TRUE)
+    t0_prob <- pred_t0@probabilities
+    t1_prob <- pred_t1@probabilities
+
+    # May need to run getValues first before running the below calculations
+
+    # Calculate change magnitude (eqn 3 in Chen 2011)
+    dP <- t1_prob - t0_prob
+    chg_mag <- sqrt(rowSums((t1_prob - t0_prob)^2))
+
+    # Calculate change vector (eqn 3 in Chen 2011). Use dot product of the base 
+    # change vectors Ea,b,. Each base change vector represents complete change 
+    # from class a to class b.
+    Eab <- diag(n_classes)
+    chg_type <- apply(dP %*% Eab, 1, function(r) which(r == max(r)))
+
+    # Now do double-window flexible pace search
+    
+}
+
+DFPS <- function(svm_t0, x_t0, radius=150) {
+    layer_name <- gsub('.shp$', '', basename(shp), ignore.case=TRUE)
+    class_polys <- readOGR(dirname(shp), layer_name, stringsAsFactors=FALSE)
+}
--- R/svm_classify.R
+++ R/svm_classify.R
@@ -4,18 +4,18 @@
 #' @param raster_file Path to the raster file.
 #' @param out_file Filename for the output shapefile.
 svm_classify <- function(raster_file, out_file) {
-#     require(e1071)
 }
 
-# require(teamr)
-# require(e1071)
-# 
 # shp <- 'H:/Data/TEAM/VB/Vectors/VB_training_1986_037_LT5.shp'
 # img_file <- 'H:/Data/TEAM/VB/Rasters/Landsat/1986_037_LT5/proc/lndsr.LT50150531986037XXX18.bsq'
+
+# require(teamr)
+# shp <- 'H:/Data/TEAM/teamr_data/L5TSR_1986_training.shp'
+# img_file <- 'H:/Data/TEAM/teamr_data/L5TSR_1986.dat'
 # rast <- brick(img_file)
 # rast <- stack(img_file, bands=as.integer(c(1, 2, 3, 4)))
 # training_data <- extract_training_data(rast, shp)
 # 
 # testSVM <- svm(y ~ ., data=training_data)
-
+# 
 # tune.svm()
