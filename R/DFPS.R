89c239aaae92a033e7cd78f4603756255bc8a353
Alex Zvoleff <azvoleff@conservation.org>
2013-09-23 13:25:48 -0400

    Finish coding DFPS.
    Also add references to SVIs.

--- R/DFPS.R
+++ R/DFPS.R
@@ -1,10 +1,14 @@
 #' Double-Window Flexible Pace Search (DFPS) threshold determination
 #'
 #' @export
-#' @param train_polys training polygons of change areas surrounded by
+#' @param chg_polys training polygons of change areas surrounded by
 #' no-change
 #' @param chg_img change magnitude image from \code{CVAPS}
 #' @param radius radius of no-change surrounding change area polygons
+#' @param delta the minimum difference between Lmax and Lmin that will allow 
+#' another pass through the search loop
+#' @param m number of potential thresholds per search iteration (see Chen et 
+#' al., 2003). Recommended to leave at default value.
 #' @references Chen, J., P. Gong, C. He, R. Pu, and P. Shi. 2003.  
 #' Land-use/land-cover change detection using improved change-vector analysis.  
 #' Photogrammetric Engineering and Remote Sensing 69:369–380.
@@ -12,5 +16,34 @@
 #' Chen, J., X. Chen, X. Cui, and J. Chen. 2011. Change vector analysis in 
 #' posterior probability space: a new method for land cover change detection.  
 #' IEEE Geoscience and Remote Sensing Letters 8:317–321.
-DFPS <- function(train_polys, chg_img, radius=150) {
+DFPS <- function(chg_polys, chg_img, radius=150, delta=.01, m=10) {
+    chg_pixels <- extract(chg_img, chg_polys)
+    nochg_polys <- gDifference(gBuffer(chg_polys, width=radius), chg_polys)
+    nochg_pixels <- extract(chg_img, nochg_polys)
+    # Calculate total number of pixels (used later)
+    A <- length(chg_pixels) + length(nochg_pixels)
+
+    # Set initial values for Lmax and Lmin that ensure the below while loop 
+    # will run.
+    Lmax <- 1
+    Lmin <- Lmax - 2 * delta
+    min_threshold <- min(chg_pixels)
+    max_threshold <- max(chg_pixels)
+    while ((Lmax - Lmin) < delta) {
+        p <- (max_threshold - min_threshold) / m
+        thresholds <- seq(min_threshold, max_threshold, p)
+        L <- c()
+        for (threshold in thresholds) {
+            A1 <- chg_pixels > threshold
+            A2 <- nochg_pixels > threshold
+            # Calculate A according to equation 4 in Chen et al. 2003
+            L <- c(L, ((A1 - A2) *100)/A)
+        }
+        kmax <- which(L == max(L))
+        min_threshold <- kmax - p
+        max_threshold <- kmax - p
+        Lmin <- min(L)
+        Lmax <- max(L)
+    }
+    return(kmax)
 }
--- R/SVIs.R
+++ R/SVIs.R
@@ -1,7 +1,7 @@
 #' Calculates the Normalized Difference Vegetation Index (NDVI)
 #'
 #' @export
-#' @param red \code{RasterLayer}
+#' @param red red \code{RasterLayer}
 #' @param nir near-infrared \code{RasterLayer}
 NDVI <- function(red, nir) {
   NDVI <- (nir - red) / (nir + red)
@@ -10,9 +10,14 @@ NDVI <- function(red, nir) {
 #' Calculates the Enhanced Vegetation Index (EVI)
 #'
 #' @export
-#' @param blue \code{RasterLayer}
-#' @param red \code{RasterLayer}
+#' @param blue blue \code{RasterLayer}
+#' @param red red \code{RasterLayer}
 #' @param nir near-infrared \code{RasterLayer}
+#' @references Huete, A. R., HuiQing Liu, and W. J. D. van Leeuwen. 1997. The 
+#' use of vegetation indices in forested regions: issues of linearity and 
+#' saturation. Pages 1966–1968 vol.4 Geoscience and Remote Sensing, 1997.  
+#' IGARSS  ’97. Remote Sensing - A Scientific Vision for Sustainable 
+#' Development., 1997 IEEE International.
 EVI <- function(blue, red, nir) {
   EVI <- 2.5*((nir - red) / (nir + 6.*red - 7.5*blue + 1.))
 }
@@ -20,18 +25,24 @@ EVI <- function(blue, red, nir) {
 #' Calculates the Modified Soil-Adjusted Vegetation Index (MSAVI)
 #'
 #' @export
-#' @param red \code{RasterLayer}
+#' @param red red \code{RasterLayer}
 #' @param nir near-infrared \code{RasterLayer}
-MSAVI2 <- function(red, nir) {
+#' @references Qi, J., A. Chehbouni, A. R. Huete, Y. H. Kerr, and S.  
+#' Sorooshian.  1994. A modified soil adjusted vegetation index. Remote Sensing 
+#' of Environment 48:119–126.
+MSAVI2 <- function( red, nir) {
   MSAVI2 <- (2.*nir + 1 - sqrt((2.*nir + 1.)^2. - 8.*(nir - red)))/2.
 }
 
 #' Calculates the Atmospherically Resistant Vegetation Index (ARVI)
 #'
 #' @export
-#' @param blue \code{RasterLayer}
-#' @param red \code{RasterLayer}
+#' @param blue blue \code{RasterLayer}
+#' @param red red \code{RasterLayer}
 #' @param nir near-infrared \code{RasterLayer}
+#' @references Kaufman, Y. J., and D. Tanre. 1996. Strategy for direct and 
+#' indirect methods for correcting the aerosol effect on remote sensing: from 
+#' AVHRR to EOS-MODIS. Remote Sensing of Environment:65–79.
 ARVI <- function(blue, red, nir) {
     ARVI <- (nir - 2.*red - blue) / (nir + 2.*red - blue)
 }
