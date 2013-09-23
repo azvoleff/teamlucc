8a54666c6e892498f87aa8382a7962f86fb5228a
Alex Zvoleff <azvoleff@conservation.org>
2013-09-23 15:11:47 -0400

    Fix so reflectances are not returned as factors.

--- R/extract_training_data.R
+++ R/extract_training_data.R
@@ -10,22 +10,21 @@
 #' the values at that location of each band in the raster stack. 
 #' @examples
 #' # TODO: Add example
-#' y <- 'H:/Data/TEAM/VB/Vectors/VB_training_1986_037_LT5.shp'
-#' img_file <- 'H:/Data/TEAM/VB/Rasters/Landsat/1986_037_LT5/proc/lndsr.LT50150531986037XXX18.bsq'
-#' x <- stack(img_file, bands=as.integer(c(1, 2, 3, 4)))
-#' extract_training_data(x, y)
+#' # y <- 'H:/Data/TEAM/VB/Vectors/VB_training_1986_037_LT5.shp'
+#' # img_file <- 'H:/Data/TEAM/VB/Rasters/Landsat/1986_037_LT5/proc/lndsr.LT50150531986037XXX18.bsq'
+#' # x <- stack(img_file, bands=as.integer(c(1, 2, 3, 4)))
+#' # train_data <- extract_training_data(x, y)
 extract_training_data <- function(x, y) {
     layer_name <- gsub('.shp$', '', basename(y), ignore.case=TRUE)
     class_polys <- readOGR(dirname(y), layer_name, stringsAsFactors=FALSE)
     if (projection(x) != projection(class_polys)) {
         stop('Coordinate systems do not match')
     }
-    pixel_sets <- extract(x, class_polys, small=TRUE)
-    # Label pixel sets
-    train_data <- mapply(function(class_type, pixel_set) 
-                         cbind("y"=rep(class_type, nrow(pixel_set)), 
-                               pixel_set), class_polys$Poly_Type, pixel_sets)
-    # Convert training data to a data.frame
-    train_data <- data.frame(abind(train_data, along=1), row.names=NULL)
-    return(train_data)
+    pixels <- extract(x, class_polys, small=TRUE, df=TRUE)
+    # Add column with class labels
+    class_polys$ID <- seq(1, nrow(class_polys))
+    pixels <- cbind(y=class_polys$Poly_Type[match(pixels$ID, class_polys$ID)], 
+                    pixels)
+    pixels <- pixels[!(names(pixels) == 'ID')]
+    return(pixels)
 }
