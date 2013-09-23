353ba8b995df7db16a51d2624ab32d4516d76ec8
Alex Zvoleff <azvoleff@conservation.org>
2013-09-23 11:44:41 -0400

    Add code to calculate SVIs.

--- R/SVIs.R
+++ R/SVIs.R
@@ -1,7 +1,37 @@
-#' Makes a polygon shapefile of image extent.
+#' Calculates the Normalized Difference Vegetation Index (NDVI)
 #'
 #' @export
-#' @param raster_file Path to the raster file.
-#' @param out_file Filename for the output shapefile.
-SVIs <- function(raster_file, out_file) {
+#' @param red \code{RasterLayer}
+#' @param nir near-infrared \code{RasterLayer}
+NDVI <- function(red, nir) {
+  NDVI <- (nir - red) / (nir + red)
+}
+
+#' Calculates the Enhanced Vegetation Index (EVI)
+#'
+#' @export
+#' @param blue \code{RasterLayer}
+#' @param red \code{RasterLayer}
+#' @param nir near-infrared \code{RasterLayer}
+EVI <- function(blue, red, nir) {
+  EVI <- 2.5*((nir - red) / (nir + 6.*red - 7.5*blue + 1.))
+}
+
+#' Calculates the Modified Soil-Adjusted Vegetation Index (MSAVI)
+#'
+#' @export
+#' @param red \code{RasterLayer}
+#' @param nir near-infrared \code{RasterLayer}
+MSAVI2 <- function(red, nir) {
+  MSAVI2 <- (2.*nir + 1 - sqrt((2.*nir + 1.)^2. - 8.*(nir - red)))/2.
+}
+
+#' Calculates the Atmospherically Resistant Vegetation Index (ARVI)
+#'
+#' @export
+#' @param blue \code{RasterLayer}
+#' @param red \code{RasterLayer}
+#' @param nir near-infrared \code{RasterLayer}
+ARVI <- function(blue, red, nir) {
+    ARVI <- (nir - 2.*red - blue) / (nir + 2.*red - blue)
 }
