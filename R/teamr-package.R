bc9dd8fe9b77b266931edfffe455c63c3a17da23
Alex Zvoleff <azvoleff@conservation.org>
2013-09-24 11:27:47 -0400

    Move package documentation to roxygen2 file.

---
+++ R/teamr-package.R
@@ -0,0 +1,34 @@
+#' TEAM Remote Sensing Processing Tools
+#'
+#' A set of scripts for processing remote sensing data to support the work of 
+#' the Tropical Ecology Assessment and Monitoring (TEAM) Network. Intended to 
+#' be used in conjunction with the set of Python scripts available in the 
+#' \href{https://github.com/azvoleff/teampy}{teampy repository} on github.
+#'
+#' @name teamr-package
+#' @aliases teamr
+#' @docType package
+#' @title TEAM Remote Sensing Processing Tools
+#' @author Alex Zvoleff, \email{azvoleff@@conservation.org}
+#' @keywords package
+NULL
+#' Training polygons for 1986 Landsat 5 Surface Reflectance image
+#' 
+#' Polygons digitized from 2001 Landsat 5 Surface Reflectance image from the 
+#' Landsat Climate Data Record archive. The training polygons can be used for 
+#' testing classification algorithms.
+#'
+#' @docType data
+#' @name L5TSR_1986_training
+#' @seealso L5TSR_2001_training
+NULL
+#' Training polygons for 2001 Landsat 5 Surface Reflectance image
+#' 
+#' Polygons digitized from 2001 Landsat 5 Surface Reflectance image from the 
+#' Landsat Climate Data Record archive. The training polygons can be used for 
+#' testing classification algorithms.
+#'
+#' @docType data
+#' @name L5TSR_2001_training
+#' @seealso L5TSR_1986_training
+NULL
