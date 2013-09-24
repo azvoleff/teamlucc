d880912aff35afabd538778c1133a4747a689d80
Alex Zvoleff <azvoleff@conservation.org>
2013-09-24 12:05:55 -0400

    Remove temporary testing code.

--- R/svm_classify.R
+++ R/svm_classify.R
@@ -12,10 +12,9 @@
 #' x_1986 <- stack(system.file('extdata/L5TSR_1986.dat', package='teamr'))
 #' data(L5TSR_1986_training)
 #' train_data_1986 <- extract_training_data(x_1986, L5TSR_1986_training)
-#' out_file_base_1986 <- 'H:/Data/TEAM/teamr_data/L5TSR_1986_classified'
-#' ## Not run ##
 #' \dontrun{
-#' svm_classify(x_1986, train_data_1986, out_file_base_1986)
+#' # This code is not run because it writes to a local folder
+#' svm_classify(x_1986, train_data_1986, 'L5TSR_1986_classified')
 #' }
 svm_classify <- function(x, train_data, out_file_base) {
     svm_tune <- tune.svm(y ~ ., data=train_data,
@@ -27,9 +26,9 @@ svm_classify <- function(x, train_data, out_file_base) {
     # per class, with the predicted probabilities of class membership for each 
     # class.
     out_classes <- raster(x)
-    out_classes <- writeStart(out_classes, paste(out_file_base, '.envi', sep=''), overwrite=TRUE)
+    out_classes <- writeStart(out_classes, paste(out_file_base, '.envi', sep=''))
     out_probs <- brick(raster(x), values=FALSE, nl=svm_best$nclasses)
-    out_probs <- writeStart(out_probs, paste(out_file_base, '_probs.envi', sep=''), overwrite=TRUE)
+    out_probs <- writeStart(out_probs, paste(out_file_base, '_probs.envi', sep=''))
     # Process over blocks (rather than row by row) to save processing time.
     bs <- blockSize(x)
     for (block_num in 1:bs$n) {
@@ -45,9 +44,3 @@ svm_classify <- function(x, train_data, out_file_base) {
     out_classes <- writeStop(out_classes)
     out_probs <- writeStop(out_probs)
 }
-# 
-# x_2001 <- stack(system.file('extdata/L5TSR_2001.dat', package='teamr'))
-# data(L5TSR_2001_training)
-# train_data_2001 <- extract_training_data(x_2001, L5TSR_2001_training)
-# out_file_base_2001 <- 'H:/Data/TEAM/teamr_data/L5TSR_2001_classified'
-# svm_classify(x_2001, y_2001, out_file_base_2001)
