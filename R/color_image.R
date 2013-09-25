9231f5d6fba133ac52fe4b2afb222ea292fc8ba3
Alex Zvoleff <azvoleff@conservation.org>
2013-09-25 14:54:42 -0400

    Add script to create classification images for viewing in ENVI/ArcINFO.

---
+++ R/color_image.R
@@ -0,0 +1,35 @@
+#' Assign class labels to classification file
+#'
+#' @export
+#' @param x classified image as \code{RasterLayer}
+#' @param cls two column matrix, where the first column is the class codes 
+#' (integers) and the second column is the class names
+#' @param out_file the filename to use for the output
+#' @references
+#' #TODO: Add examples
+#' #x <- raster('L5TSR_1986_to_2001_chgtraj.envi')
+#' #cls <- cbind(traj_lut$Traj_Code,
+#' #             paste(traj_lut$t0_name, traj_lut$t1_name, sep='-'))
+#' #outfile <- 'L5TSR_1986_to_2001_chgtraj_colored.envi'
+#' #color_image(img, cls, outfile)
+color_image <- function(x, cls, outfile) {
+    # Reclassify image so it is coded from 0 - length(cls[1]). ENVI and 
+    # other classification file formats rely on the codes being sequential, 
+    # starting at zero.
+    x <- reclassify(x, cbind(cls[, 1], seq(0, length(cls[, 1]) - 1)))
+    x.sp <- as(x, "SpatialPixelsDataFrame")
+    cls_colors <- t(col2rgb(cls[, 1]))
+    # Select appropriate data type and missing value tag depending on data 
+    # attributes.
+    if (max(traj_lut$Traj_Code > 254)) {
+        gdaltype <- 'Int16'
+        gdalmvFlag <- -32768
+    } else {
+        gdaltype <- 'Byte'
+        gdalmvFlag <- 255
+    }
+    writeGDAL(x.sp, outfile, drivername="ENVI", type=gdaltype,
+              colorTable=list(cls_colors), catNames=list(cls[, 2]),
+              mvFlag=gdalmvFlag)
+}
+
