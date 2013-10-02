683523419e2ba72aa5d71968623e97bd5b0e156d
Alex Zvoleff <azvoleff@conservation.org>
2013-10-02 10:04:29 -0400

    Add new script to calculate statistics on change trajectories.

---
+++ R/chg_traj_stats.R
@@ -0,0 +1,20 @@
+#' Calculate change-trajectory statistics
+#'
+#' @export
+#' @param chg_traj a list (as output by \code{chg_traj} with two elements: 
+#' traj_lut (a lookup table of change trajectory codes) and chg_traj (a 
+#' \code{RasterLayer} of change trajectory codes.
+#' @return a \code{data.frame} object with change trajectory statistics
+#' @examples
+#' #TODO: Add examples
+chg_traj_stats <- function(chg_traj) {
+    chg_table <- table(getValues(traj$chg_traj))
+    summ_table <- data.frame(Traj_Code=traj$traj_lut$Code,
+                             Trajectory=paste(traj$traj_lut$t0_name, traj_lut$traj_lut$t1_name, 
+                                              sep='-'))
+    summ_table <- cbind(summ_table, n_pixels=chg_table[match(row.names(chg_table), summ_table$Traj_Code)])
+    row.names(summ_table) <- NULL
+    summ_table$Frac_Chg <- summ_table$n_pixels / sum(summ_table$n_pixels)
+    summ_table$Frac_Tot <- summ_table$n_pixels / length(traj$chg_traj)
+    return(summ_table)
+}
