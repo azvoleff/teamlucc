4e5d5452347b6dbaf5b3dcbbcb681d110f627252
Alex Zvoleff <azvoleff@conservation.org>
2013-10-07 14:18:52 -0400

    Finalize glcm function.

--- R/glcm.R
+++ R/glcm.R
@@ -6,42 +6,33 @@
 #' @param window the window size to consider for texture calculation as a two 
 #' element integer vector
 #' @param shift a 2 element integer vector giving the shift (Q in Gonzalez and 
-#' Woods, 2008)
-#' @param statistic A list of GLCM texture measures to calculate.  Can include 
-#' any (one or more) of the following: 'mean', 'variance', 'homogeneity', 
-#' 'contrast', 'dissimilarity', 'entropy', 'second_moment', 'correlation'.
+#' Woods, 2008). 
+#' @param statistics A list of GLCM texture measures to calculate.  Can include 
+#' any (one or more) of the following: 'mean', 'variance', 'covariance', 
+#' 'homogeneity', 'contrast', 'dissimilarity', 'entropy', 'second_moment', 
+#' 'correlation'.
 #' @return A /code{RasterLayer} with the calculated requested GLCM texture 
 #' measures.
 #' @references Lu, D., and M. Batistella. 2005. Exploring TM image texture and 
 #' its relationships with biomass estimation in Rondônia, Brazilian Amazon.  
-#' Acta Amazonica 35:249–257.
+#' Acta Amazonica 35:249-257.
 #'
 #' Gonzalez, R. C. 2008. Digital image processing. 3rd ed. Prentice Hall, Upper 
-#' Saddle River, N.J, page 830-836.
+#' Saddle River, N.J, pages 830-836.
 #'
 #' Haralick, R. M., K. Shanmugam, and I. Dinstein. 1973. Textural features for 
 #' image classification. IEEE Transactions on Systems, Man and Cybernetics 
-#' SMC-3:610 –621.
+#' SMC-3:610-621.
+#'
+#' Pratt, W. K. 2007. Digital image processing: PIKS Scientific inside4th ed., 
+#' Newly updated and rev. ed. Wiley-Interscience, Hoboken, N.J pages 540-541, 
+#' 563-566.
 #' @examples
-library(teamr)
-L5TSR_1986 <- stack(system.file('extdata/L5TSR_1986.dat', package='teamr'))
-x <- raster(L5TSR_1986, layer=1)
-n_grey <- 3
-window <- c(3, 3)
-shift <- c(1, 1)
-statistic <- 'mean'
-x <- raster(matrix(c(0, 0, 0, 0, 0,
-              0, 0, 0, 2, 0,
-              0, 0, 2, 2, 0,
-              1, 1, 2, 3, 0,
-              1, 1, 2, 3, 0), nrow=5))
-
-
+#' #TODO: Code examples
 glcm <- function(x, n_grey=32, window=c(3, 3), shift=c(1, 1),
-                 statistic='mean') {
-    # Define shift[2] to be in the North (up) direction:
-    shift[2] <- -shift[2]
-
+                 statistics=c('mean', 'variance', 'covariance', 'homogeneity', 
+                             'contrast', 'dissimilarity', 'entropy', 
+                             'second_moment', 'correlation')) {
     if (length(window) != 2) {
         stop('window must be integer vector of length 2')
     }
@@ -59,25 +50,33 @@ glcm <- function(x, n_grey=32, window=c(3, 3), shift=c(1, 1),
     x_grey <- cut(x, breaks=seq(cellStats(x, 'min'), cellStats(x, 'max'), 
                                 length.out=n_grey + 1), include.lowest=TRUE)
 
-    rast <- matrix(getValues(x_grey), nrow=5)[c(1:3), c(2:4)]
+    # DEBUG ###############
+    #rast <- matrix(getValues(x_grey), nrow=5)[c(1:4), c(2:5)]
+    # DEBUG ###############
 
-    # Setup coordinates for offset image
-    col_coords <- matrix(rep(shift[1] + seq(1, window[2]), window[1]), 
-                       nrow=window[1], byrow=TRUE)
-    row_coords <- matrix(rep(shift[2] + seq(1, window[1]), window[2]), 
-                       nrow=window[1])
-    # Set coordinates that fall outside the window to NA
-    col_coords[col_coords > window[2] | col_coords < 1] <- NA
-    row_coords[row_coords > window[1] | row_coords < 1] <- NA
-    col_coords[is.na(row_coords)] <- NA
-    row_coords[is.na(col_coords)] <- NA
+    # Calculate column major indices for base and offset images
+    ul_base <- c(1, 1)
+    if (shift[1] < 0) {ul_base[1] <- ul_base[1] + abs(shift[1])}
+    if (shift[2] < 0) {ul_base[2] <- ul_base[2] + abs(shift[2])}
+    ul_offset <- ul_base + shift
+    base_cols <- matrix(rep(ul_base[2] + seq(1, window[2]), window[1]) - 1, 
+                         nrow=window[1], byrow=TRUE)
+    base_rows <- matrix(rep(ul_base[1] + seq(1, window[1]), window[2]) - 1, 
+                         nrow=window[1])
+    offset_cols <- matrix(rep(ul_offset[2] + seq(1, window[2]), window[1]) - 1, 
+                         nrow=window[1], byrow=TRUE)
+    offset_rows <- matrix(rep(ul_offset[1] + seq(1, window[1]), window[2]) - 1, 
+                         nrow=window[1])
+    offset_indices <- (offset_cols - 1) * (window[1]+abs(shift[1])) + offset_rows
+    base_indices <- (base_cols - 1) * (window[1]+abs(shift[1])) + base_rows
 
-    calc_texture <- function(rast, statistics, col_coords, row_coords, ...) {
+    calc_texture <- function(rast, statistics, base_indices, offset_indices, n_grey, 
+                             ...) {
         G <- matrix(0, n_grey, n_grey)
-        co_occur <- cbind(as.vector(rast), rast[cbind(as.vector(row_coords), 
-                                                      as.vector(col_coords))])
+        co_occur <- cbind(rast[,,1][base_indices], rast[,,1][offset_indices])
         for (n in 1:nrow(co_occur)) {
-            G[co_occur[n, 1], co_occur[n, 2]] <- G[co_occur[n, 1], co_occur[n, 2]] + 1
+            G[co_occur[n, 1], co_occur[n, 2]] <- G[co_occur[n, 1],
+                                                   co_occur[n, 2]] + 1
         }
         Pij = G / sum(G)
 
@@ -89,80 +88,87 @@ glcm <- function(x, n_grey=32, window=c(3, 3), shift=c(1, 1),
         mr = sum(c(1:nrow(Pij)) * rowsum)
         mc = sum(c(1:ncol(Pij)) * colsum)
         sig2r = sum((c(1:nrow(Pij)) - mr)^2 * rowsum)
-        sig2c = sum((c(1:nrow(Pij)) - mc)^2 * colsum)
+        sig2c = sum((c(1:ncol(Pij)) - mc)^2 * colsum)
 
+        # Make a matrix of i's and a matrix of j's to be used in the below 
+        # matrix calculations. These matrices are the same shape as Pij with 
+        # the entries equal to the i indices of each cell (for the imat matrix, 
+        # which is indexed over the rows) or the j indices of each cell (for 
+        # the jmat matrix, which is indexed over the columns).
+        imat <- matrix(rep(1:nrow(Pij), ncol(Pij)), nrow=nrow(Pij))
+        jmat <- matrix(rep(1:ncol(Pij), nrow(Pij)), ncol=ncol(Pij), byrow=TRUE)
+
+        textures <- c()
         if ('mean' %in% statistics) {
+            # Defined as in Lu and Batistella, 2005, page 252
+            textures <- c(textures, mr)
         }
         if ('variance' %in% statistics) {
+            # Defined as in Haralick, 1973, page 619 (equation 4)
+            textures <- c(textures, sum(rowSums((imat - mr)^2 * Pij)))
+        }
+        if ('covariance' %in% statistics) {
+            # Defined as in Pratt, 2007, page 540
+            textures <- c(textures, sum(rowSums((imat - mr) *
+                                                (jmat - mc) * Pij)))
         }
         if ('homogeneity' %in% statistics) {
+            # Defined as in Gonzalez and Woods, 2009, page 832
+            textures <- c(textures, sum(rowSums(Pij / (1 + abs(imat - jmat)))))
         }
         if ('contrast' %in% statistics) {
+            # Defined as in Gonzalez and Woods, 2009, page 832
+            textures <- c(textures, sum(rowSums((imat - jmat)^2 * Pij)))
         }
         if ('dissimilarity' %in% statistics) {
+            #TODO: Find source for dissimilarity
+            stop('Error: dissimilarity not yet supported')
         }
         if ('entropy' %in% statistics) {
+            # Defined as in Gonzalez and Woods, 2009, page 832
+            textures <- c(textures, -sum(rowSums(Pij * log2(Pij + .001))))
         }
         if ('second_moment' %in% statistics) {
+            # Defined as in Haralick, 1973, page 619
+            textures <- c(textures, sum(rowSums(Pij^2)))
         }
         if ('correlation' %in% statistics) {
+            # Defined as in Gonzalez and Woods, 2009, page 832
+            textures <- c(textures, sum(rowSums(((imat - mr) * (jmat - mc) * Pij) /
+                                                (sqrt(sig2r) * sqrt(sig2c)))))
         }
     }
-    textures <- rasterEngine(x_grey, fun=calc_texture,
-                             args=list(statistics, col_coords, row_coords), window_dims=window)
-    
-    return(textures)
+
+    texture_img <- rasterEngine(rast=x_grey, fun=calc_texture,
+                             args=list(statistics=statistics, 
+                                       base_indices=base_indices, 
+                                       offset_indices=offset_indices,
+                                       n_grey=n_grey), 
+                             window_dims=(window + abs(shift)))
+    names(texture_img) <- statistics 
+    texture_img <- setMinMax(texture_img)
+
+    return(texture_img)
 }
-a  <- matrix(seq(1, 25), nrow=5)
-a[c(3:5),c(3:5)]
-# Encode matrix from Fig. 3 in Haralick (1973) for testing:
-#a <- matrix(c(0, 0, 0, 2, 0, 0, 2, 2, 1, 1, 2, 3, 1, 1, 2, 3), nrow=4)
-a <- matrix(c(0, 0, 0, 0, 0,
-              0, 0, 0, 2, 0,
-              0, 0, 2, 2, 0,
-              1, 1, 2, 3, 0,
-              1, 1, 2, 3, 0), nrow=5)
+# DEBUG ####################
+library(teamr)
+L5TSR_1986 <- stack(system.file('extdata/L5TSR_1986.dat', package='teamr'))
+x <- raster(L5TSR_1986, layer=1)
+# x <- raster(matrix(c(0, 0, 0, 0, 0,
+#               0, 0, 0, 2, 0,
+#               0, 0, 2, 2, 0,
+#               1, 1, 2, 3, 0,
+#               1, 1, 2, 3, 0), nrow=5))
+statistics <- c('mean', 'variance', 'covariance', 'homogeneity', 'contrast', 
+                'entropy', 'second_moment', 'correlation')
+window <- c(3, 3)
+shift <- c(1, 1)
+n_grey <- 32
 
-# n_grey is the number of grey levels in the image
-n_grey <- 4
-# d is distance value (in pixels)
-d <- 2
-# w is number of pixels to consider in each direction (horizontal and 
-# vertical), and including the center pixel, but not taking into account d (for 
-# this see 's' below).
-w <- 3
-# s is the total number of pixels needed in each direction (vertical upwards 
-# and horizontal to the right) for all comparisons to be valid and to not fall 
-# outside of the original image matrix.
-s <- (w + d)
-
-# o_x is origin x value (center x coordinate)
-o_x <- 3
-# o_y is origin y value (center y coordinate)
-o_y <- 3
-
-# First make a matrix with the coordinates of all the pixels in the window, 
-# relative to the coordinates of the central pixel, considered to be at (0, 0).
-w_x <- matrix(rep(seq(-w, w), s), nrow=s, byrow=TRUE)
-w_y <- matrix(rep(seq(-w, w), s), nrow=s, byrow=FALSE)
-
-# Now make a matrix with the coordinates for a 0 degree neighbor, the neighbor 
-# immediately to the left of each pixel.
-nb_x_0 <- w_x + 1
-nb_y_0 <- w_y
-
-w_vals <- a[cbind(as.vector(w_y + o_y), as.vector(w_x + o_x))]
-nb_0 <- a[cbind(as.vector(nb_y_0 + o_y), as.vector(nb_x_0 + o_x))]
-nb_45 <- a[cbind(as.vector(nb_y_45 + o_y), as.vector(nb_x_45 + o_x))]
-nb_90 <- a[cbind(as.vector(nb_y_90 + o_y), as.vector(nb_x_90 + o_x))]
-nb_135 <- a[cbind(as.vector(nb_y_135 + o_y), as.vector(nb_x_135 + o_x))]
-
-occ[w_vals, nb_0] <- occ[w_vals, nb_0] + 1
-occ[w_vals, nb_45] <- occ[w_vals, nb_45] + 1
-occ[w_vals, nb_90] <- occ[w_vals, nb_90] + 1
-occ[w_vals, nb_135] <- occ[w_vals, nb_135] + 1
-
-# Make symmetric to account for forward and backward relationships:
-occ[upper.tri(occ)] <- occ[upper.tri(occ)] + t(occ)[upper.tri(occ)]
-occ[lower.tri(occ)] <- t(occ)[lower.tri(occ)]
-occ[diag(nrow(occ))] <- 2 * diag(occ)
+sfQuickInit(4)
+timing <- proc.time()
+textures <- glcm(x, statistics=statistics)
+print(proc.time() - timing)
+writeRaster(textures, 'L5TSR_1986_GLCM_TEXTURES_mine.envi')
+sfQuickStop()
+# DEBUG ####################
