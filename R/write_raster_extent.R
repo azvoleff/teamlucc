80251c01068212d30a3d8063a8efd6cd6bd7da13
Alex Zvoleff <azvoleff@conservation.org>
2013-11-05 17:17:09 -0500

    Allow inclusion of arbitrarily named fields in output shapefile.
    Useful for creating training datasets based on a multitemporal layer stack.

--- R/write_raster_extent.R
+++ R/write_raster_extent.R
@@ -3,8 +3,14 @@
 #' @export
 #' @import rgdal
 #' @param x A \code{Raster*} object
-#' @param out_file Filename for the output shapefile.
-write_raster_extent <- function(x, out_file) {
+#' @param out_file Filename for the output shapefile
+#' @param fields list of fields to include in the output shapefile
+write_raster_extent <- function(x, out_file, fields=c('t1_class',
+                                                      't2_class')) {
+    if ((length(out_file) > 1) || (!grepl('.shp$', out_file, 
+                                          ignore.case=TRUE))) {
+        stop("out_file must be a file path ending in '.shp'")
+    }
     img_ext <- extent(x)
     # Need to create the shapefile with a polygon in it as R doesn't have an 
     # easy way to create and setup an empty shapefile. So make a polygon with 
@@ -18,9 +24,12 @@ write_raster_extent <- function(x, out_file) {
                                                     img_ext@ymin)))), ID=1)
     Sr <- SpatialPolygons(list(img_ext_poly), 
                           proj4string=CRS(proj4string(x)))
+    # Create a data.frame with empty rows for each field in field, and with the 
+    # 'Notes' field set to 'Extent'
+    out_data <- data.frame(t(c(rep('', length(fields)), 'Extent')))
+    names(out_data) <- c(fields, 'Notes')
     # Now create the training data SpatialPolygonsDataFrame instance
-    train_df <- SpatialPolygonsDataFrame(Sr, data=data.frame(Poly_Type='Extent', 
-                                                             Notes=''))
+    train_df <- SpatialPolygonsDataFrame(Sr, data=data.frame(out_data))
     layer_name <- gsub('.shp$', '', basename(out_file), ignore.case=TRUE)
     writeOGR(train_df, dirname(out_file), layer_name, driver='ESRI Shapefile')
 }
