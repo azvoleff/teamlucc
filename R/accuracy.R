56fad11e7b00c69e02d9fbcb2c02721e1cb8f206
Alex Zvoleff <azvoleff@conservation.org>
2013-11-06 12:27:02 -0500

    Add stub for calculating classification accuracy statistics.

---
+++ R/accuracy.R
@@ -0,0 +1,10 @@
+#' Calculate statistics on classification accuracy
+#'
+#' TODO: Write docs
+#'
+#' @export
+#' @param x classification results
+#' @return list of accuracy statistics
+chg_dir <- function(x) {
+    #TODO: Code function
+}
