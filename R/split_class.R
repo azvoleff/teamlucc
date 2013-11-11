a2a705229fb4001dbb3565fe3532ae44281979a7
Alex Zvoleff <azvoleff@gmail.com>
2013-11-11 13:02:46 -0500

    Add stub to split classes using a cluster algorithm.
    Useful for classes with high variance due to differentiation within the class - such as agricultural cover types where different crops may have different spectral characteristics.

---
+++ R/split_class.R
@@ -0,0 +1,11 @@
+#' Calculate change-trajectory image
+#'
+#' @export
+#' @import mclust
+#' @param ignorepersistence whether to ignore persistence of a class (if 
+#' @return list object with two elements: 'clusts', a vector of cluster 
+#' assignments for the clustered training data, and 'Mclust', the output from 
+#' running \code{Mclust} from the \code{Mclust} package.
+split_class <- function(train_data) {
+    #TODO: Mclust()
+}
