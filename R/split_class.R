fa9f24380c25519ac2bfa322cf03f4242416a9d3
Alex Zvoleff <azvoleff@gmail.com>
2013-11-11 13:24:30 -0500

    Update to use importFrom for Mclust.

--- R/split_class.R
+++ R/split_class.R
@@ -1,7 +1,7 @@
 #' Calculate change-trajectory image
 #'
 #' @export
-#' @import mclust
+#' @importFrom mclust Mclust
 #' @param ignorepersistence whether to ignore persistence of a class (if 
 #' @return list object with two elements: 'clusts', a vector of cluster 
 #' assignments for the clustered training data, and 'Mclust', the output from 
