57bf1dfb8252d9c43ad6dad554b84a54181f1a83
Alex Zvoleff <azvoleff@gmail.com>
2013-12-06 15:31:14 -0500

    Update glcm tests, removing old debugging code.

--- tests/testthat/test-glcm.R
+++ tests/testthat/test-glcm.R
@@ -1,68 +1,62 @@
-context("GLCM texture calculations")
+context("GLCM textures")
+
+library('raster')
 
 # Make a function to get 2d matrix from 3d matrix returned by glcm
-get_glcm_texture <- function(statistic, ...) {
+get_teamr_glcm_texture <- function(statistic) {
     if (length(statistic) != 1) {
         stop('length of statistic must be equal to 1')
     }
-    texture <- glcm(glcm_test_raster, 32, c(3, 3), c(1, 1), statistic, ...)
-    return(as.array(texture)[,,1])
+    texture <- glcm(glcm_test_raster, 32, c(3, 3), c(1, 1), statistic)
+    return(getValues(texture))
 }
 
 # Test all statistics that are available in EXELIS ENVI match the textures 
 # output by teamr
-statistics <- c('mean_ENVI', 'variance_ENVI', 'homogeneity', 'contrast', 
-                'dissimilarity', 'entropy', 'second_moment', 'correlation')
-
-# glcm_test_raster_ENVI_textures contains the results from running ENVI on the 
-# glcm_test_raster. The below tests ensure that the results from teamr match 
-# those from EXELIS ENVI.
-textures_teamr <- glcm(glcm_test_raster, 32, c(3, 3), c(1, 1), statistics)
-
 test_that("GLCM mean is correct", {
-    expect_equal(get_glcm_texture('mean_ENVI'),
-                 expected=as.matrix(glcm_test_raster_ENVI_textures$mean),
+    expect_equal(get_teamr_glcm_texture('mean_ENVI'),
+                 expected=getValues(glcm_test_raster_ENVI_textures$mean),
                  tolerance=.000001)
 })
 
 test_that("GLCM variance is correct", {
-    expect_equal(get_glcm_texture('variance_ENVI'),
-                 expected=as.matrix(glcm_test_raster_ENVI_textures$variance),
+    expect_equal(get_teamr_glcm_texture('variance_ENVI'),
+                 expected=getValues(glcm_test_raster_ENVI_textures$variance),
                  tolerance=.000001)
 })
 
 test_that("GLCM homogeneity is correct", {
-    expect_equal(get_glcm_texture('homogeneity'),
-                 expected=as.matrix(glcm_test_raster_ENVI_textures$homogeneity),
+    expect_equal(get_teamr_glcm_texture('homogeneity'),
+                 expected=getValues(glcm_test_raster_ENVI_textures$homogeneity),
                  tolerance=.000001)
 })
 
 test_that("GLCM contrast is correct", {
-    expect_equal(get_glcm_texture('contrast'),
-                 expected=as.matrix(glcm_test_raster_ENVI_textures$contrast),
+    expect_equal(get_teamr_glcm_texture('contrast'),
+                 expected=getValues(glcm_test_raster_ENVI_textures$contrast),
                  tolerance=.000001)
 })
 
 test_that("GLCM dissimilarity is correct", {
-    expect_equal(get_glcm_texture('dissimilarity'),
-                 expected=as.matrix(glcm_test_raster_ENVI_textures$dissimilarity),
+    expect_equal(get_teamr_glcm_texture('dissimilarity'),
+                 expected=getValues(glcm_test_raster_ENVI_textures$dissimilarity),
                  tolerance=.000001)
 })
 
 test_that("GLCM entropy is correct", {
-    expect_equal(get_glcm_texture('entropy'),
-                 expected=as.matrix(glcm_test_raster_ENVI_textures$entropy),
+    expect_equal(get_teamr_glcm_texture('entropy'),
+                 expected=getValues(glcm_test_raster_ENVI_textures$entropy),
                  tolerance=.000001)
 })
 
 test_that("GLCM second_moment is correct", {
-    expect_equal(get_glcm_texture('second_moment'),
-                 expected=as.matrix(glcm_test_raster_ENVI_textures$second_moment),
+    expect_equal(get_teamr_glcm_texture('second_moment'),
+                 expected=getValues(glcm_test_raster_ENVI_textures$second_moment),
                  tolerance=.000001)
 })
 
 test_that("GLCM correlation is correct", {
-    expect_equal(get_glcm_texture('correlation'),
-                 expected=as.matrix(glcm_test_raster_ENVI_textures$correlation),
+    expect_equal(get_teamr_glcm_texture('correlation'),
+                 expected=getValues(glcm_test_raster_ENVI_textures$correlation),
                  tolerance=.000001)
 })
