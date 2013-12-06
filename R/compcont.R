5441daccae33513c5db0b0afd60e9080ef45634e
Alex Zvoleff <azvoleff@gmail.com>
2013-12-06 14:13:36 -0500

    Fix compcont.R stub arguments and reference.

--- R/compcont.R
+++ R/compcont.R
@@ -4,16 +4,12 @@
 #' composite operator recommended by Pontius and Cheuk (2006).
 #'
 #' @export
-#' @param model a classification model with a \code{predict} method
-#' @param test_data a training/testing dataset as output by the 
-#' \code{extract_training_data} function. If a 'Training' column is included 
-#' \code{accuracy} will use only the data not used in training for evaluating 
-#' model accuracy.
-#' @return contingency table
-#' @references Pontius, R. G., and M. Millones. 2011. Death to Kappa: birth of 
-#' quantity disagreement and allocation disagreement for accuracy assessment.  
-#' International Journal of Remote Sensing 32:4407-4429.
-compcont <- function(cont_t, ref_map=NULL, comp_map=NULL) {
+#' @return matrix with contingency table
+#' @references Pontius, R. G., and M. L. Cheuk. 2006.  A generalized 
+#' cross-tabulation matrix to compare soft-classified maps at multiple 
+#' resolutions.  International Journal of Geographical Information Science 
+#' 20:1-30.
+compcont <- function() {
     stop('compcont is not yet finished')
     #TODO: finish coding this function
 }
