236bc2392a4faf3a101adbc8e0735dcdf1f2fc9c
Alex Zvoleff <azvoleff@gmail.com>
2014-01-06 16:40:37 -0500

    Move DEM processing to a separate function.

--- R/team_preprocess_landsat.R
+++ R/team_preprocess_landsat.R
@@ -4,36 +4,35 @@
 #' @importFrom rgeos gContains gUnion
 #' @param image_list a list of paths to Landsat CDR images that have been 
 #' converted to bsq files with teampy.
-#' @param dem a list of digital elevation models (DEMs) that (when mosaiced) 
-#' covers the full extent of all the images in the image_list.
+#' @param dem path to a digital elevation model (DEM) covering the full extent 
+#' of all the images in \code{image_list}. See \code{team_setup_dem} for a 
+#' function simplifying this.
+#' @param slopeaspect path to a two layer raster stack with slope and aspect 
+#' calculated from the above DEM
 #' @param n_cpus the number of CPUs to use for processes that can run in 
 #' parallel
 #' @param cleartmp whether to clear temp files on each run through the loop
 #' @examples
 #' \dontrun{
-#' image_list <- list('H:/Data/TEAM/VB/Rasters/Landsat/1986_037_LT5/proc/lndsr.LT50150531986037XXX18')
-#' dem_list <- list('H:/Data/TEAM/VB/Rasters/DEM/ASTER/ASTGTM2_N09W084_dem.tif',
-#'                  'H:/Data/TEAM/VB/Rasters/DEM/ASTER/ASTGTM2_N09W085_dem.tif',
-#'                  'H:/Data/TEAM/VB/Rasters/DEM/ASTER/ASTGTM2_N10W084_dem.tif',
-#'                  'H:/Data/TEAM/VB/Rasters/DEM/ASTER/ASTGTM2_N09W085_dem.tif')
-#' team_preprocess(image_list, dem_list)
+#' image_list <- c('H:/Data/TEAM/VB/Rasters/Landsat/1986_037_LT5/proc/lndsr.LT50150531986037XXX18.bsq',
+#'                 'H:/Data/TEAM/VB/Rasters/Landsat/2001_014_LT5/proc/lndsr.LT50150532001014AAA01.bsq',
+#'                 'H:/Data/TEAM/VB/Rasters/Landsat/2012_021_LE7/proc/lndsr.LE70150532012021EDC00.bsq')
+#' dem <- 'H:/Data/TEAM/VB/LCLUC_Analysis/VB_dem_mosaic.envi'
+#' slopeaspect <- 'H:/Data/TEAM/VB/LCLUC_Analysis/VB_dem_mosaic_slopeaspect.envi'
+#' team_preprocess(image_list, dem, 'H:/Data/TEAM/VB/LCLUC_Analysis', 3, TRUE)
 #' }
-team_preprocess <- function(image_list, DEM, output_dir, n_cpus=2, 
+team_preprocess <- function(image_list, DEM, slopeaspect, output_dir, n_cpus=2, 
                             cleartmp=FALSE) {
+    if (n_cpus > 1) sfQuickInit(n_cpus)
     ################################################################################
     # Verify extents and projections of images and DEMs match (DEM projection 
     # doesn't have to match image projection, but all DEMs must have the same 
     # projections, and all images must have the same projections).
     image_stacks <- lapply(image_list, stack)
-    dem_rasts <- lapply(dem_list, raster)
 
     image_prj <- projection(image_stacks[[1]])
     if (any(lapply(image_stacks, projection) != image_prj)) {
         stop("each image in image_list must have the same projection")
-    }
-    dem_prj <- projection(dem_rasts[[1]])
-    if (any(lapply(dem_rasts, projection) != dem_prj)) {
-        stop("each DEM in dem_list must have the same projection")
     }
 
     #TODO: download ASTER that aligns with CDR SR imagery
@@ -45,90 +44,74 @@ team_preprocess <- function(image_list, DEM, output_dir, n_cpus=2,
     # of the images in image_list
     image_extent_polys <- lapply(image_stacks, get_extent_poly)
     # Make sure the DEM extents and image extent polys are in same projection 
-    dem_extent_polys <- lapply(dem_rasts, function(rast) get_extent_poly(projectExtent(rast, CRS(image_prj))))
-    dem_mosaic_extent <- dem_extent_polys[[1]]
-    for (dem_extent_poly in dem_extent_polys[-1]) {
-        dem_mosaic_extent <- gUnion(dem_mosaic_extent, dem_extent_poly)
+    dem <- raster(dem)
+    dem_extent_poly <- get_extent_poly(dem)
+    extents_contained <- unlist(lapply(image_extent_polys, function(ext) gContains(dem_extent_poly, ext)))
+    for (n in 1:length(extents_contained)) {
+        warning(paste("DEM does not fully cover extent of", image_list[n]))
     }
-    extents_contained <- unlist(lapply(image_extent_polys, function(ext) gContains(dem_mosaic_extent, ext)))
-    if (!any(extents_contained)) {
-        warning("DEM does not fully cover extent of images in image_list")
-    }
-
-    ################################################################################
-    # Mosaic DEMs
-    print('Mosaicing DEMs...')
-    trackTime(action='start')
-    # See http://bit.ly/1dJPIeF re issue in raster that necessitates below workaround
-    # TODO: Contact Hijmans re possible fix
-    mosaicargs <- dem_rasts
-    mosaicargs$fun <- mean
-    dem_mosaic <- do.call(mosaic, mosaicargs)
-    dem_mosaic_filename <- file.path(data_dir, 'Rasters/DEM/ASTER/ASTER_DEM_L5TSR_1986_mosaic.envi')
-    dem_mosaic <- projectRaster(dem_mosaic, crs=CRS(image_prj), dem_mosaic_filename)
-    #dem_mosaic <- mosaic(dem_rasts, fun='mean')
-    trackTime()
-
-    print('Running slopeasp_seq...')
-    trackTime(action='start')
-    slopeaspect_filename <- file.path(data_dir, 'Rasters/DEM/ASTER/ASTER_DEM_L5TSR_1986_slopeaspect.envi')
-    slopeaspect <- slopeasp_seq(dem_mosaic, filename=slopeaspect_filename, overwrite=TRUE)
-    trackTime()
 
     for (image_path in image_list) {
+        print(paste("Processing", image_path))
+
+        ################################################################################
+        # Extract image basename for use in naming subsequent files
+        base_regex <- '((19[89])|(20[01]))[0-9]_[0-9]{3}_((LT[45])|(LE[78]))'
+        base_regexpr <- regexpr(base_regex, image_path)
+        image_basename <- substr(image_path, base_regexpr,
+                                 base_regexpr+attr(base_regexpr, "match.length")-1)
+        
         ################################################################################
         # Load data and mask out clouds and missing values
         print('Loading data and masking clouds and missing data...')
         trackTime(action='start')
-        image_rast_original_file <- file.path(data_dir, paste0(img_basename, '.bsq'))
         data_bands <- grep('band_[123457]_reflectance',  
-                           get_band_names_from_hdr(extension(image_rast_original_file, '.hdr')))
-        image_rast <- stack(image_rast_original_file, bands=data_bands)
-        names(image_rast) <- get_band_names_from_hdr(extension(image_rast_original_file, '.hdr'))[data_bands]
+                           get_band_names_from_hdr(extension(image_path, '.hdr')))
+        image_rast <- stack(image_path, bands=data_bands)
+        names(image_rast) <- get_band_names_from_hdr(extension(image_path, '.hdr'))[data_bands]
         # Rename bands with shorter names. "sr" stands for "surface reflectance"
         names(image_rast) <- gsub('_reflectance', 'sr', names(image_rast))
         names(image_rast) <- gsub('band_', 'b', names(image_rast))
 
         mask_bands <- grep('(combined_cloud_mask)|(missing_mask)', 
-                           get_band_names_from_hdr(extension(image_rast_original_file, '.hdr')))
-        masks <- stack(image_rast_original_file, bands=mask_bands)
-        names(masks) <- get_band_names_from_hdr(extension(image_rast_original_file, '.hdr'))[mask_bands]
+                           get_band_names_from_hdr(extension(image_path, '.hdr')))
+        masks <- stack(image_path, bands=mask_bands)
+        names(masks) <- get_band_names_from_hdr(extension(image_path, '.hdr'))[mask_bands]
 
         # The combined cloud mask includes the cloud_QA, cloud_shadow_QA, and 
         # adjacent_cloud_QA layers. Missing or clouded pixels are coded as 0, while 
         # good pixels are coded as 1.
-        image_rast <- image_rast * masks$combined_cloud_mask * masks$missing_mask
+        image_rast_mask <- masks$combined_cloud_mask * masks$missing_mask
+        image_rast_mask_path <- file.path(output_path, paste(sitecode, image_basename, 'mask.envi', sep='_'))
+        writeRaster(image_rast_mask, image_rast_mask_path)
 
-        mask_file_path <- file.path(data_dir, paste0(img_basename, '_masked.envi'))
-        writeRaster(image_rast, mask_file_path)
+        image_rast <- mask(image_rast, image_rast_mask, maskvalue=0)
+        image_rast_masked_path <- file.path(output_path, paste(sitecode, image_basename, 'masked.envi', sep='_'))
+        writeRaster(image_rast, image_rast_masked_path)
+
+        # image_rast_mask is no longer needed, so unload it to save memory
+        rm(image_rast_mask)
+
         trackTime()
 
         ################################################################################
         # Perform topographic correction
         print('Cropping slope/aspect raster to extent of Landsat image...')
         trackTime(action='start')
-        beginCluster(2)
-        matched_DEM_file <- file.path(data_dir, 'Rasters/DEM/ASTER/ASTER_DEM_L5TSR_1986.grd')
-        dem_mosaic <- match_rasters(image_rast, dem_mosaic, filename=matched_DEM_file)
-        endCluster()
-        names(dem_mosaic) <- 'elevation'
-        # Need to detach snow or else sfQuickInit will not work
-        detach('package:snow', unload=TRUE)
+        cropped_dem_file <- file.path(output_path, paste(sitecode, image_basename, 'dem.envi', sep='_'))
+        cropped_slopeaspect_file <- file.path(output_path, paste(sitecode, image_basename, 'dem_slopeaspect.envi', sep='_'))
+        cropped_dem <- match_rasters(image_rast, dem, filename=cropped_dem_file)
+        cropped_slopeapect <- match_rasters(image_rast, slopeaspect, filename=cropped_dem_file)
         trackTime()
 
         print('Running topocorr...')
         library(spatial.tools)
-        sfQuickInit(3)
         trackTime(action='start')
-        image_rast_original_file <- file.path(data_dir, paste0(img_basename, '.bsq'))
-        slopeaspect_filename <- file.path(data_dir, 'Rasters/DEM/ASTER/ASTER_DEM_L5TSR_1986_slopeaspect.grd')
-        mask_file_path <- file.path(data_dir, paste0(img_basename, '_masked.envi'))
-        slopeaspect <- brick(slopeaspect_filename)
-        image_rast <- brick(mask_file_path)
-        metadatafile <- extension(image_rast_original_file, 'txt')
+        slopeaspect <- brick(slopeaspect)
+        metadatafile <- extension(image_path, 'txt')
         sunelev <- 90 - as.numeric(get_metadata_item(metadatafile, 'SolarZenith'))
         sunazimuth <- as.numeric(get_metadata_item(metadatafile, 'SolarAzimuth'))
-        topocorr_filename <- file.path(data_dir, 'Rasters/Landsat/1986_037_LT5/proc/L5TSR_1986_topocorr.grd')
+        topocorr_filename <- file.path(output_path, paste(sitecode, image_basename, 'masked_tc.envi', sep='_'))
         # First draw a sample for the Minnaert k regression
         horizcells <- 10
         vertcells <- 10
@@ -144,14 +127,13 @@ team_preprocess <- function(image_list, DEM, output_dir, n_cpus=2,
                                        inparallel=TRUE, overwrite=TRUE,
                                        sampleindices=sampleindices)
         trackTime()
-        sfQuickStop()
 
         ################################################################################
         # Calculate additional predictor layers (MSAVI and textures)
 
         print('Calculating MSAVI2...')
         trackTime(action='start')
-        MSAVI2_filename <- file.path(data_dir, 'Rasters/Landsat/1986_037_LT5/proc/L5TSR_1986_MSAVI.envi')
+        MSAVI2_filename <- file.path(output_path, paste(sitecode, image_basename, 'masked_tc_MSAVI2.envi', sep='_'))
         MSAVI2_layer <- MSAVI2(red=raster(image_rast, layer=3),
                                nir=raster(image_rast, layer=4))
         writeRaster(MSAVI2_layer, MSAVI2_filename)
@@ -159,7 +141,7 @@ team_preprocess <- function(image_list, DEM, output_dir, n_cpus=2,
 
         print('Calculating GLCM textures from MSAVI image...')
         trackTime(action='start')
-        MSAVI2_glcm_filename <- file.path(data_dir, 'Rasters/Landsat/1986_037_LT5/proc/L5TSR_1986_MSAVI2_glcm.envi')
+        MSAVI2_glcm_filename <- file.path(output_path, paste(sitecode, image_basename, 'masked_tc_MSAVI2_glcm.envi', sep='_'))
         MSAVI2_layer <- raster(MSAVI2_filename)
         MSAVI2_glcm <- glcm(MSAVI2_layer, filename=glcm_filename, overwrite=TRUE)
         trackTime()
@@ -176,12 +158,12 @@ team_preprocess <- function(image_list, DEM, output_dir, n_cpus=2,
                                   MSAVI2_glcm$glcm_mean,
                                   MSAVI2_glcm$glcm_variance,
                                   MSAVI2_glcm$glcm_dissimilarity,
-                                  dem_mosaic,
+                                  dem,
                                   slopeaspect$slope,
                                   slopeaspect$aspect)
-        image_rast_preds_filename <- file.path(data_dir, 'Rasters/Landsat/1986_037_LT5/proc/L5TSR_1986_preds.envi')
+        image_rast_preds_filename <- file.path(output_path, paste(sitecode, image_basename, 'predictors.envi', sep='_'))
         writeRaster(image_rast_preds, image_rast_preds_filename)
         if (cleartmp) removeTmpFiles(h=1)
     }
-
+    if (n_cpus > 1) sfQuickStop()
 }
---
+++ R/team_setup_dem.R
@@ -0,0 +1,58 @@
+#' Setup the DEM mosaic for a particular TEAM site
+#'
+#' @export
+#' @import spatial.tools
+#' @param dem a list of digital elevation models (DEMs) that (when mosaiced) 
+#' covers the full extent of all the images in the image_list.
+#' @param sitecode code to use as a prefix for all filenames
+#' @param output_path the path to use for the output 
+#' @param sample_image a \code{Raster*} or the path to an image in a format 
+#' readable by \code{raster}. The sample image will be used to set the 
+#' projection system for the output DEM mosaic.
+#' @param n_cpus the number of CPUs to use for processes that can run in 
+#' parallel
+#' @examples
+#' \dontrun{
+#' dem_list <- list('H:/Data/TEAM/VB/Rasters/DEM/ASTER/ASTGTM2_N09W084_dem.tif',
+#'                  'H:/Data/TEAM/VB/Rasters/DEM/ASTER/ASTGTM2_N09W085_dem.tif',
+#'                  'H:/Data/TEAM/VB/Rasters/DEM/ASTER/ASTGTM2_N10W084_dem.tif',
+#'                  'H:/Data/TEAM/VB/Rasters/DEM/ASTER/ASTGTM2_N09W085_dem.tif')
+#' team_setup_dem(dem_list, "VB", 'H:/Data/TEAM/VB/LCLUC_Analysis/')
+#' }
+team_setup_dem <- function(dem_list, sitecode, output_path, sample_image=NULL, 
+                            n_cpus=1) {
+    if (n_cpus > 1) sfQuickInit(n_cpus)
+    ################################################################################
+    # Verify projections of DEMs match
+    dem_rasts <- lapply(dem_list, raster)
+
+    dem_prj <- projection(dem_rasts[[1]])
+    if (any(lapply(dem_rasts, projection) != dem_prj)) {
+        stop("each DEM in dem_list must have the same projection")
+    }
+
+    ################################################################################
+    # Mosaic DEMs
+    print('Mosaicing DEMs...')
+    trackTime(action='start')
+    # See http://bit.ly/1dJPIeF re issue in raster that necessitates below workaround
+    # TODO: Contact Hijmans re possible fix
+    mosaicargs <- dem_rasts
+    mosaicargs$fun <- mean
+    dem_mosaic <- do.call(mosaic, mosaicargs)
+    dem_mosaic_filename <- file.path(output_path, paste0(sitecode, '_dem_mosaic.envi'))
+    sample_image <- raster(sample_image)
+    if (is.null(sample_image) | (projection(sample_image) == projection(dem_mosaic))) {
+        writeRaster(dem_mosaic, dem_mosaic_filename)
+    } else {
+        dem_mosaic <- projectRaster(dem_mosaic, sample_image, filename=dem_mosaic_filename)
+    }
+    trackTime()
+
+    print('Running slopeasp_seq...')
+    trackTime(action='start')
+    slopeaspect_filename <- file.path(output_path, paste0(sitecode, '_dem_mosaic_slopeaspect.envi'))
+    slopeaspect <- slopeasp_seq(dem_mosaic, filename=slopeaspect_filename, overwrite=TRUE)
+    trackTime()
+    if (n_cpus > 1) sfQuickStop()
+}
