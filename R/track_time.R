37a9d768b30a74bcff5ad81f45ee92f562ef677a
Alex Zvoleff <azvoleff@gmail.com>
2014-01-07 15:36:24 -0500

    Rename tracktime for consistency.
    Also have return strings rather than print directly.

--- R/track_time.R
+++ R/track_time.R
@@ -8,23 +8,23 @@
 #' started.
 #' @examples
 #' #TODO: Add examples
-trackTime <- function(label='', action='print') {
+track_time <- function(label='', action='print') {
     if (action == 'start') {
         assign(paste('trackTime_starttime', label, sep='_'), now(), 
                inherits=TRUE)
         if (label != '') {
-            print(paste0('trackTime: Tracking timer "', label,'" started at ', now(), '.'))
+            return(paste0('trackTime: Tracking timer "', label,'" started at ', now(), '.'))
         } else {
-            print(paste0('trackTime: Tracking timer started at ', now(), '.'))
+            return(paste0('trackTime: Tracking timer started at ', now(), '.'))
         }
     } else if (action == 'print') {
         startTimeVar <- mget(paste('trackTime_starttime', label, sep='_'), 
                              ifnotfound=list(NULL), inherits=TRUE)
         if (!is.null(startTimeVar[[1]])) {
             if (label != '') {
-                print(paste0('trackTime (', label, '): ', round(as.duration(now() - startTimeVar[[1]]), 2)))
+                return(paste0('trackTime (', label, '): ', round(as.duration(now() - startTimeVar[[1]]), 2)))
             } else {
-                print(paste('trackTime:', round(as.duration(now() - startTimeVar[[1]]), 2)))
+                return(paste('trackTime:', round(as.duration(now() - startTimeVar[[1]]), 2)))
             }
         } else {
             stop(paste0('No timer with label "', label, '" found. Must call trackTime(action="start") before calling trackTime()'))
