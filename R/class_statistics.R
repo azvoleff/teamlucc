2e05c193789f1167bf83475b9998000fa7af3a01
Alex Zvoleff <azvoleff@gmail.com>
2014-01-07 17:00:17 -0500

    Fix plyr in NAMESPACE.

--- R/class_statistics.R
+++ R/class_statistics.R
@@ -1,7 +1,7 @@
 #' Exports statistics on pixels within each of a set of land cover classes
 #'
 #' @export
-#' @importFrom plyr ddply
+#' @importFrom plyr ddply summarize .
 #' @importFrom reshape2 melt
 #' @param x A \code{RasterLayer} from which class statistics will be 
 #' calculated.
