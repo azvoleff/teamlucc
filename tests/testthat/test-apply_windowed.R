8fe715720882380aae066196b48456787d023aa6
Alex Zvoleff <azvoleff@gmail.com>
2014-01-08 16:08:20 -0500

    Add a tolerance accounting for machine precision.
    Also rename test file for consistency with past commits.

--- tests/testthat/test-apply_windowed.R
+++ tests/testthat/test-apply_windowed.R
@@ -8,7 +8,8 @@ max_x <- cellStats(x, 'max')
 # Test that glcm calculated with apply_windowed matches the output from running 
 # glcm directly
 glcm_apply_windowed <- apply_windowed(x, glcm, edge=c(1, 2), min_x=min_x, 
-                                    max_x=max_x, statistics=c('mean', 'variance'))
+                                      max_x=max_x,
+                                      statistics=c('mean', 'variance'))
 # Force glcm_apply_windowed to be stored in memory so it can compare 
 # identically with glcm_glcm
 glcm_apply_windowed <- glcm_apply_windowed + 0
@@ -17,15 +18,16 @@ glcm_glcm <- glcm(x, statistics=c('mean', 'variance'))
 glcm_glcm <- glcm_glcm + 0
 
 test_that("glcm calculated with apply_windowed matches glcm calculated directly", {
-    expect_equal(glcm_apply_windowed, expected=glcm_glcm)
+    expect_equal(glcm_apply_windowed, expected=glcm_glcm, tolerance=1e-7)
 })
 
 ###############################################################################
 # Test that glcm calculated with apply_windowed matches the output from running 
 # glcm directly when only a single texture measure is calculated
 
-glcm_apply_windowed_single <- apply_windowed(x, glcm, edge=c(1, 2), min_x=min_x, 
-                                           max_x=max_x, statistics=c('mean'))
+glcm_apply_windowed_single <- apply_windowed(x, glcm, edge=c(1, 2), 
+                                             min_x=min_x, max_x=max_x, 
+                                             statistics=c('mean'))
 # Force glcm_apply_windowed to be stored in memory so it can compare 
 # identically with glcm_glcm
 glcm_apply_windowed_single <- glcm_apply_windowed_single + 0
@@ -33,5 +35,6 @@ glcm_apply_windowed_single <- glcm_apply_windowed_single + 0
 glcm_glcm_single <- glcm(x, statistics=c('mean'))
 
 test_that("glcm calculated with apply_windowed matches glcm calculated directly", {
-    expect_equal(glcm_apply_windowed_single, expected=glcm_glcm_single)
+    expect_equal(glcm_apply_windowed_single, expected=glcm_glcm_single, 
+                 tolerance=1e-7)
 })
