9dd578a0e8cd5716f69209c19932bb5b98c6d40e
Alex Zvoleff <azvoleff@gmail.com>
2014-01-10 11:26:20 -0500

    Refine match_rasters.R args.

--- R/match_rasters.R
+++ R/match_rasters.R
@@ -8,12 +8,11 @@
 #' the /code{baseimg}. The /code{matchimg} will then be cropped and extended to 
 #' match the extent of the /code{baseimg}.
 #' @param filename file on disk to save \code{Raster*} to (optional)
-#' @param datatype the datatype to use when saving to \code{filename}
-#' (optional)
-#' @param overwrite whether to overwrite \code{filename} if it already exists
 #' @param method the method to use if projection is needed to match image 
 #' coordinate systems, or if resampling is needed to align image origins. Can 
 #' be "ngb" for nearest-neighbor, or "binlinear" for bilinear interpolation
+#' @param ... additional arguments to pass to \code{writeRaster} (such as 
+#' datatype and filename)
 #' @return The /code{matchimg} reprojected (if necessary), cropped, and 
 #' extended to match the /code{baseimg}.
 #' @details Note that if the \code{matchimg} needs to be reprojected,
@@ -25,8 +24,8 @@
 #' 
 #' # Crop and extend the DEM mosaic to match the Landsat image
 #' matched_DEM <- match_rasters(L5TSR_1986, DEM_mosaic)
-match_rasters <- function(baseimg, matchimg, filename, datatype, 
-                          overwrite=FALSE, method='bilinear') {
+match_rasters <- function(baseimg, matchimg, filename, method='bilinear',
+                          ...) {
     if (projection(baseimg) != projection(matchimg)) {
         matchimg <- projectRaster(from=matchimg, to=baseimg, method=method)
     }
@@ -41,13 +40,7 @@ match_rasters <- function(baseimg, matchimg, filename, datatype,
     }
 
     if (!missing(filename)) {
-        if (!missing(datatype)) {
-            matchimg <- writeRaster(matchimg, filename, 
-                                    overwrite=overwrite, datatype=datatype)
-        } else {
-            matchimg <- writeRaster(matchimg, filename=filename, 
-                                    overwrite=overwrite)
-        }
+            matchimg <- writeRaster(matchimg, filename=filename, ...)
     }
     return(matchimg)
 }
