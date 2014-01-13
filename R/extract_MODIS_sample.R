ee4c804667fb5c0c36c1888e72f2344c5454073c
Alex Zvoleff <azvoleff@gmail.com>
2014-01-13 10:41:20 -0500

    Add new script to extract LEDAPS CDR hdf files using gdalUtils.

---
+++ R/extract_MODIS_sample.R
@@ -0,0 +1,7 @@
+#' Generate training data at Landsat scale from a MODIS GLCT image.
+#'
+#' @export
+#' @param x a map of land cover classes as a \code{RasterLayer}
+extract_MODIS_sample <- function(x, y) {
+    #TODO: Write this function.
+}
---
+++ R/unstack_ledapscdr.R
@@ -0,0 +1,57 @@
+#' Convert LEDAPS CDR Landsat format images from HDF4 to ENVI format
+#'
+#' This function converts a Landsat surface reflectance (SR) image from the 
+#' Landsat Climate Data Record (CDR) archive into a series of single band 
+#' images in ENVI format.
+#'
+#' @export
+#' @importFrom gdalUtils gdal_translate
+#' @importFrom tools file_path_sans_ext
+#' @param x input HDF4 file
+#' @param out output file
+#' @param overwrite whether to overwrite existing files
+unstack_ledapscdr <- function(x, out, overwrite=FALSE) {
+    if (!file_test('-d', out)) {
+        stop('out must be a directory')
+    }
+    out_basename <- file_path_sans_ext(basename(x))
+
+    # Dump the main HDF metadata to a text file
+    metadata <- gdalinfo(x)
+    start_row <- grep('Metadata:', metadata) + 1
+    items <- c()
+    item_values <- c()
+    for (n in start_row:length(metadata)) {
+        this_row <- metadata[n]
+        if (!grepl('^ ', this_row)) break
+        this_row <- strsplit(gsub('^[ ]*', '', this_row), split='=')
+        items <- c(items, this_row[[1]][1])
+        item_values <- c(item_values, this_row[[1]][2])
+    }
+    metadata_df <- data.frame(item=items, value=item_values)
+    write.table(metadata_df,
+                file=paste0(file.path(out, out_basename), '_metadata.txt'), sep=',', 
+                row.names=FALSE, col.names=FALSE)
+
+    sds <- get_subdatasets(x)
+    loc <- regexpr('[a-zA-Z0-9_-]*$', sds)
+    out_rasts <- c()
+    for (n in 1:length(sds)) {
+        start_char <- loc[n]
+        stop_char <- start_char + attr(loc, 'match.length')[n]
+        band_name <- substr(sds[[n]], start_char, stop_char)
+        this_out <- paste0(file.path(out, out_basename), '_', band_name, '.envi')
+        if (file.exists(this_out) & overwrite) {
+            unlink(this_out)
+            if(file.exists(extension(this_out, 'hdr'))) 
+                unlink(extension(this_out, 'hdr'))
+            if(file.exists(extension(this_out, 'envi.aux.xml'))) 
+                unlink(extension(this_out, 'envi.aux.xml'))
+            if(file.exists(extension(this_out, 'envi.enp'))) 
+                unlink(extension(this_out, 'envi.enp'))
+        }
+        out_rast <- gdal_translate(x, of="ENVI", sd_index=n, this_out, 
+                                   outRaster=TRUE)
+        out_rasts <- list(out_rasts, out_rast)
+    }
+}
