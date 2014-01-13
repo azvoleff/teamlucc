a1707cbff0d20f5a974d0ecc2533c05c23f06b08
Alex Zvoleff <azvoleff@gmail.com>
2014-01-13 17:19:01 -0500

    Update metadata reading and file loading.

--- R/team_preprocess_landsat.R
+++ R/team_preprocess_landsat.R
@@ -1,12 +1,12 @@
 #' Preprocess surface reflectance imagery from the Landsat CDR archive
 #'
 #' @export
-#' @import spatial.tools
+#' @importFrom spatial.tools sfQuickInit sfQuickStop
 #' @importFrom rgeos gContains gUnion
-#' @param image_list a list of paths to a set of Landsat CDR images (for a 
-#' single TEAM site) that have been converted to bsq files with teampy.
+#' @param image_dirs list of paths to a set of Landsat CDR image files in ENVI 
+#' format as output by the \code{unstack_ledapscdr} function.
 #' @param dem path to a digital elevation model (DEM) covering the full extent 
-#' of all the images in \code{image_list}. See \code{team_setup_dem} for a 
+#' of all the images in \code{image_dirs}. See \code{team_setup_dem} for a 
 #' function simplifying this.
 #' @param slopeaspect path to a two layer raster stack with slope and aspect 
 #' calculated from the above DEM
@@ -21,33 +21,86 @@
 #' \code{notify} function should accept a string as the only argument.
 #' @examples
 #' \dontrun{
-#' image_list <- c('H:/Data/TEAM/VB/Rasters/Landsat/1986_037_LT5/proc/lndsr.LT50150531986037XXX18_metadata.txt',
-#'                 'H:/Data/TEAM/VB/Rasters/Landsat/2001_014_LT5/proc/lndsr.LT50150532001014AAA01_metadata.txt',
-#'                 'H:/Data/TEAM/VB/Rasters/Landsat/2012_021_LE7/proc/lndsr.LE70150532012021EDC00_metadata.txt')
+#' image_dirs <- c('H:/Data/TEAM/VB/Rasters/Landsat/1986_037_LT5/proc',
+#'                 'H:/Data/TEAM/VB/Rasters/Landsat/2001_014_LT5/proc',
+#'                 'H:/Data/TEAM/VB/Rasters/Landsat/2012_021_LE7/proc')
 #' dem <- 'H:/Data/TEAM/VB/LCLUC_Analysis/VB_dem_mosaic.envi'
 #' slopeaspect <- 'H:/Data/TEAM/VB/LCLUC_Analysis/VB_dem_mosaic_slopeaspect.envi'
-#' team_preprocess(image_list, dem, slopeaspect, "VB", 
+#' team_preprocess(image_dirs, dem, slopeaspect, "VB", 
 #' 'H:/Data/TEAM/VB/LCLUC_Analysis', 3, TRUE)
 #' }
-team_preprocess_landsat <- function(image_list, dem, slopeaspect, sitecode, 
+team_preprocess_landsat <- function(image_dirs, dem, slopeaspect, sitecode, 
                                     output_path, n_cpus=1, cleartmp=FALSE, 
                                     overwrite=FALSE, notify=print) {
     notify("Starting preprocessing...")
     if (n_cpus > 1) sfQuickInit(n_cpus)
+
+    # Setup a regex to identify Landsat CDR images
+    lndsr_regex <- '^lndsr.((LT4)|(LT5)|(LE7)|(LE8))[0-9]{6}[12][0-9]{6}[a-zA-Z]{3}[0-9]{2}'
+
+    image_bands <- c('band1', 'band2', 'band3', 'band4', 'band5', 'band7')
+    mask_bands <- c('fill_QA', 'fmask_band')
+    image_files <- c()
+    image_stacks <- c()
+    mask_stacks <- c()
+    for (image_dir in image_dirs) {
+        if (!file_test("-d", image_dir)) {
+            stop(paste(image_dir, "does not exist"))
+        }
+        lndsr_files <- dir(image_dir, pattern=lndsr_regex)
+        loc <- regexpr(lndsr_regex, lndsr_files)
+        stop_char <- loc + attr(loc, 'match.length') - 1
+        image_basenames <- unique(substr(lndsr_files, loc, stop_char))
+
+        if (length(image_basenames) == 0) {
+            stop(paste('no files found in', image_dir))
+        }
+
+        for (image_basename in image_basenames) {
+            band_files <- c()
+            for (image_band in image_bands) {
+                band_files <- c(band_files,
+                                paste(file.path(image_dir, image_basename), 
+                                      image_band, sep='_'))
+            }
+            band_files <- paste0(band_files, '.envi')
+            image_stack <- stack(band_files)
+            names(image_stack) <- image_bands
+            image_files <- c(image_files, list(band_files))
+            # Read gain and offset from the metadata file as raster does not 
+            # support passing as.is=TRUE to readGDAL, meaning that GDAL 
+            # automatically scales the returned data turning it into floats.
+            gain(image_stack) <- 1/as.numeric(get_metadata_item(band_files[1], 'scale_factor'))
+            offs(image_stack) <- as.numeric(get_metadata_item(band_files[1], 'add_offset'))
+
+            mask_band_files <- c()
+            for (mask_band in mask_bands) {
+                mask_band_files <- c(mask_band_files,
+                                     paste(file.path(image_dir, 
+                                                     image_basename), 
+                                           mask_band, sep='_'))
+            }
+            mask_band_files <- paste0(mask_band_files, '.envi')
+            mask_stack <- stack(mask_band_files)
+            names(mask_stack) <- mask_bands
+
+            image_stacks <- c(image_stacks, image_stack)
+            mask_stacks <- c(mask_stacks, mask_stack)
+        }
+    }
+
     ##########################################################################
     # Verify extents and projections of images and DEMs match (DEM projection 
     # doesn't have to match image projection, but all DEMs must have the same 
     # projections, and all images must have the same projections).
-    image_stacks <- lapply(image_list, stack)
-
     image_prj <- projection(image_stacks[[1]])
     if (any(lapply(image_stacks, projection) != image_prj)) {
-        stop("each image in image_list must have the same projection")
+        stop("each input image must have the same projection")
     }
 
     #TODO: download ASTER that aligns with CDR SR imagery
     # Verify the combined extent of the DEMs in dem_list covers the full area 
-    # of the images in image_list
+    # of the images in image_stacks
     image_extent_polys <- lapply(image_stacks, get_extent_poly)
     if ('RasterLayer' != class(dem)) dem <- raster(dem)
     # Make sure the DEM extents and image extent polys are in same projection 
@@ -55,59 +108,55 @@ team_preprocess_landsat <- function(image_list, dem, slopeaspect, sitecode,
     extents_contained <- unlist(lapply(image_extent_polys,
                                        function(ext) gContains(dem_extent_poly, ext)))
     for (n in 1:length(extents_contained)) {
-        warning(paste("DEM does not fully cover extent of", image_list[n]))
+        warning(paste("DEM does not fully cover extent of", image_files[n]))
     }
 
-    for (image_path in image_list) {
-        notify(paste("Processing", image_path))
+    for (n in 1:length(image_stacks)) {
+        image_stack <- image_stacks[[n]]
+        mask_stack <- mask_stacks[[n]]
+        band1_imagefile <- image_files[[n]][1]
 
         ######################################################################
         # Determine image basename for use in naming subsequent files
-        aq_date <- get_metadata_item(extension(image_path, '.txt'), 'AcquisitionDate')
+        aq_date <- get_metadata_item(band1_imagefile, 'AcquisitionDate')
         aq_date <- strptime(aq_date, format="%Y-%m-%dT%H:%M:%OSZ")
-        short_name  <- get_metadata_item(extension(image_path, '.txt'), 'ShortName')
+        short_name  <- get_metadata_item(band1_imagefile, 'ShortName')
         image_basename <- paste(format(aq_date, '%Y_%j'), short_name, sep='_')
+
+        notify(paste0("Processing ", image_basename, '...'))
 
         ######################################################################
         # Load data and mask out clouds and missing values
         notify('Loading data and masking clouds and missing data...')
         notify(track_time(action='start'))
-        data_bands <- grep('band_[123457]_reflectance',  
-                           get_band_names_from_hdr(extension(image_path, '.hdr')))
-        image_rast <- stack(image_path, bands=data_bands)
-        names(image_rast) <- get_band_names_from_hdr(extension(image_path, '.hdr'))[data_bands]
-        # Rename bands with shorter names. "sr" stands for "surface reflectance"
-        names(image_rast) <- gsub('_reflectance', 'sr', names(image_rast))
-        names(image_rast) <- gsub('band_', 'b', names(image_rast))
-
-        mask_bands <- grep('(combined_cloud_mask)|(missing_mask)', 
-                           get_band_names_from_hdr(extension(image_path, '.hdr')))
-        masks <- stack(image_path, bands=mask_bands)
-        names(masks) <- get_band_names_from_hdr(extension(image_path, '.hdr'))[mask_bands]
 
         # The combined cloud mask includes the cloud_QA, cloud_shadow_QA, and 
         # adjacent_cloud_QA layers. Missing or clouded pixels are coded as 0, while 
         # good pixels are coded as 1.
-        image_rast_mask_path <- file.path(output_path,
+        image_stack_mask_path <- file.path(output_path,
                                           paste(sitecode, image_basename, 
                                                 'mask.envi', sep='_'))
-        image_rast_mask <- overlay(masks,
-                                   fun=function(cloud_mask, missing_mask) {
-                                       cloud_mask * missing_mask
-                                   },
-                                   filename=image_rast_mask_path, 
-                                   overwrite=overwrite, 
-                                   datatype='INT2S')
+        # fmask_band key:
+        # 	0 = clear
+        # 	1 = water
+        # 	2 = cloud_shadow
+        # 	3 = snow
+        # 	4 = cloud
+        # 	255 = fill value
+        # fill_QA key:
+        # 	0 = not fill
+        # 	255 = fill
+        image_stack_mask <- overlay(mask_stack$fmask_band, mask_stack$fill_QA,
+                                    fun=function(fmask, fill) {
+                                     ((fmask == 0) | (fmask == 1)) & (fill == 0)
+                                    })
 
-        image_rast_masked_path <- file.path(output_path,
+        image_stack_masked_path <- file.path(output_path,
                                             paste(sitecode, image_basename, 
                                                   'masked.envi', sep='_'))
-        image_rast <- mask(image_rast, image_rast_mask,
-                           filename=image_rast_masked_path, 
-                           overwrite=overwrite, datatype=dataType(image_rast)[1])
-        # image_rast_mask is no longer needed, so unload it to save memory
-        rm(image_rast_mask)
-
+        image_stack <- mask(image_stack, image_stack_mask, maskvalue=0,
+                            filename=image_stack_masked_path, 
+                            overwrite=overwrite, datatype=dataType(image_stack)[1])
         notify(track_time())
 
         ######################################################################
@@ -117,7 +166,7 @@ team_preprocess_landsat <- function(image_list, dem, slopeaspect, sitecode,
         cropped_dem_file <- file.path(output_path,
                                       paste(sitecode, image_basename, 
                                             'dem.envi', sep='_'))
-        cropped_dem <- match_rasters(image_rast, dem, 
+        cropped_dem <- match_rasters(image_stack, dem, 
                                      filename=cropped_dem_file, 
                                      overwrite=overwrite,
                                      datatype=dataType(dem))
@@ -129,7 +178,7 @@ team_preprocess_landsat <- function(image_list, dem, slopeaspect, sitecode,
                                                     'dem_slopeaspect.envi', 
                                                     sep='_'))
         #TODO: Check why extend in this match_rasters call is raising warning
-        cropped_slopeaspect <- match_rasters(image_rast, slopeaspect, 
+        cropped_slopeaspect <- match_rasters(image_stack, slopeaspect, 
                                              filename=cropped_slopeaspect_file, 
                                              overwrite=overwrite, 
                                              datatype=dataType(slopeaspect)[1])
@@ -145,12 +194,11 @@ team_preprocess_landsat <- function(image_list, dem, slopeaspect, sitecode,
         # Note that rowmajor indices are needed as raster layers are stored in 
         # rowmajor order, unlike most R objects that are addressed in column 
         # major order
-        sampleindices <- gridsample(image_rast, horizcells=10, vertcells=10, 
+        sampleindices <- gridsample(image_stack, horizcells=10, vertcells=10, 
                                     nsamp=nsamp, rowmajor=TRUE)
 
-        metadatafile <- extension(image_path, 'txt')
-        sunelev <- 90 - as.numeric(get_metadata_item(metadatafile, 'SolarZenith'))
-        sunazimuth <- as.numeric(get_metadata_item(metadatafile, 'SolarAzimuth'))
+        sunelev <- 90 - as.numeric(get_metadata_item(band1_imagefile, 'SolarZenith'))
+        sunazimuth <- as.numeric(get_metadata_item(band1_imagefile, 'SolarAzimuth'))
 
         topocorr_filename <- file.path(output_path,
                                        paste(sitecode, image_basename, 
@@ -160,7 +208,7 @@ team_preprocess_landsat <- function(image_list, dem, slopeaspect, sitecode,
         } else {
             inparallel <- FALSE
         }
-        image_rast <- topographic_corr(image_rast, cropped_slopeaspect, sunelev, 
+        image_stack <- topographic_corr(image_stack, cropped_slopeaspect, sunelev, 
                                        sunazimuth, method='minnaert_full', 
                                        filename=topocorr_filename, 
                                        inparallel=inparallel, 
@@ -175,8 +223,8 @@ team_preprocess_landsat <- function(image_list, dem, slopeaspect, sitecode,
         MSAVI2_filename <- file.path(output_path,
                                      paste(sitecode, image_basename, 
                                            'masked_tc_MSAVI2.envi', sep='_'))
-        MSAVI2_layer <- MSAVI2(red=raster(image_rast, layer=3),
-                               nir=raster(image_rast, layer=4))
+        MSAVI2_layer <- MSAVI2(red=raster(image_stack, layer=3),
+                               nir=raster(image_stack, layer=4))
         MSAVI2_layer <- writeRaster(MSAVI2_layer, MSAVI2_filename, 
                                     overwrite=overwrite, datatype=dataType(MSAVI2_layer))
         notify(track_time())
@@ -197,26 +245,24 @@ team_preprocess_landsat <- function(image_list, dem, slopeaspect, sitecode,
 
         ######################################################################
         # Layer stack predictor layers:
-        image_rast_preds <- stack(raster(image_rast, layer=1),
-                                  raster(image_rast, layer=2),
-                                  raster(image_rast, layer=3),
-                                  raster(image_rast, layer=4),
-                                  raster(image_rast, layer=5),
-                                  raster(image_rast, layer=6),
-                                  MSAVI2_layer,
-                                  MSAVI2_glcm$glcm_mean,
-                                  MSAVI2_glcm$glcm_variance,
-                                  MSAVI2_glcm$glcm_dissimilarity,
-                                  cropped_dem,
-                                  cropped_slopeaspect$slope,
-                                  cropped_slopeaspect$aspect)
-        image_rast_preds_filename <- file.path(output_path,
-                                               paste(sitecode, image_basename, 
-                                                     'predictors.envi', 
-                                                     sep='_'))
-        image_rast_preds <- writeRaster(image_rast_preds, 
-                                        image_rast_preds_filename, 
-                                        overwrite=overwrite)
+        predictors <- stack(raster(image_stack, layer=1),
+                            raster(image_stack, layer=2),
+                            raster(image_stack, layer=3),
+                            raster(image_stack, layer=4),
+                            raster(image_stack, layer=5),
+                            raster(image_stack, layer=6),
+                            MSAVI2_layer,
+                            MSAVI2_glcm$glcm_mean,
+                            MSAVI2_glcm$glcm_variance,
+                            MSAVI2_glcm$glcm_dissimilarity,
+                            cropped_dem,
+                            cropped_slopeaspect$slope,
+                            cropped_slopeaspect$aspect)
+        predictors_filename <- file.path(output_path,
+                                         paste(sitecode, image_basename, 
+                                               'predictors.envi', sep='_'))
+        predictors <- writeRaster(predictors, predictors_filename, 
+                                  overwrite=overwrite)
         if (cleartmp) removeTmpFiles(h=1)
     }
     if (n_cpus > 1) sfQuickStop()
