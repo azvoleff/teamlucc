8f58c32fadb2e1ad6d28779ea7553ff4116803f0
Alex Zvoleff <azvoleff@gmail.com>
2014-01-14 14:07:23 -0500

    Refactor linear_stretch.R and add tests.

--- R/linear_stretch.R
+++ R/linear_stretch.R
@@ -1,3 +1,12 @@
+.do_stretch <- function(x, pct, max_val) {
+    lower <- quantile(x, prob=0 + pct/100, na.rm=TRUE)
+    upper <- quantile(x, prob=1 - pct/100, na.rm=TRUE)
+    x[x > upper] <- upper
+    x[x < lower] <- lower
+    x <- ((x - lower) / (upper-lower)) * max_val
+    return(x)
+}
+
 #' Apply a linear stretch to an image
 #'
 #' Applies a linear stretch to an image (default linear 2% stretch), and 
@@ -14,15 +23,20 @@ linear_stretch <- function(x, pct=2, max_val=1, ...) {
     # Applies linear stretch (2 percent by default). Assumes image is arranged 
     # with bands in columns. Returns the image with stretch applied and bands 
     # rescaled to range from 0 - max_val.
-    if ((pct < 0) | pct > 100) {
-        stop('pct must be > 0 and < 100')
+    if ((pct < 0) | pct >= 50) {
+        stop('pct must be > 0 and < 50')
     }
-    for (n in 1:ncol(x)) {
-        pct2 <- quantile(x[, n], prob=0 + pct/100, na.rm=TRUE)
-        pct98 <- quantile(x[, n], prob=1 - pct/100, na.rm=TRUE)
-        x[, n] <- ((x[, n] - pct2) / pct98) * max_val
-        x[, n][x[, n] < 0] <- 0
-        x[, n][x[, n] > 1] <- 1
+    if (class(x) %in% c('RasterLayer')) {
+        x <- setValues(x, .do_stretch(getValues(x), pct, max_val))
+        return(x)
+    } else if (class(x) %in% c('RasterStack', 'RasterBrick')) {
+        for (n in 1:nlayers(x)) {
+            x <- setValues(x, .do_stretch(getValues(raster(x, layer=n)),
+                                          pct, max_val), layer=n)
+        }
+        return(x)
+    } else if (is.null(dim(x)) || (length(dim(x)) == 2) || (dim(x)[3] == 1)) {
+        # Handle a vector, matrix, or n x m x 1 array
+        return(.do_stretch(x, pct, max_val))
     }
-    return(x)
 }
---
+++ tests/testthat/test-linear_stretch.R
@@ -0,0 +1,49 @@
+context("linear stretch")
+
+test_vals <- seq(1:9)
+
+expected_numeric <- c(0.0000000, 0.1093750, 0.2395833, 0.3697917, 0.5000000, 
+                      0.6302083, 0.7604167, 0.8906250, 1.0000000)
+expected_matrix <- matrix(expected_numeric, nrow=3)
+expected_RasterLayer <- raster(matrix(expected_numeric, nrow=3))
+expected_RasterStack <- stack(expected_raster, expected_raster)
+
+test_RasterLayer <- raster(matrix(test_vals, nrow=3))
+test_RasterStack <- stack(test_RasterLayer, test_RasterLayer + 10)
+
+test_that("stretch works for numeric", {
+    expect_equal(linear_stretch(test_vals), expected=expected_numeric, 
+                 tolerance=1e-6)
+})
+
+test_that("stretch works for matrix", {
+    expect_equal(linear_stretch(matrix(test_vals, nrow=3)), 
+                 expected=expected_matrix, tolerance=1e-6)
+})
+
+test_that("stretch works for RasterLayer", {
+    expect_equal(linear_stretch(test_RasterLayer),
+                 expected=expected_RasterLayer, tolerance=1e-6)
+})
+
+test_that("stretch works for RasterStack", {
+    expect_equal(linear_stretch(test_RasterStack),
+                 expected=expected_RasterStack, tolerance=1e-6)
+})
+
+test_that("stretch works for different max_val", {
+    expect_equal(linear_stretch(test_vals, max_val=255),
+                 expected=expected_numeric * 255, tolerance=1e-6)
+})
+
+expected_numeric_pct10 <- c(0.00000, 0.03125, 0.18750, 0.34375, 0.50000, 0.65625, 
+                      0.81250, 0.96875, 1.00000)
+test_that("stretch works for different pct", {
+    expect_equal(linear_stretch(test_vals, pct=10),
+                 expected=expected_numeric_pct10, tolerance=1e-6)
+})
+
+test_that("stretch works for different pct and max_val", {
+    expect_equal(linear_stretch(test_vals, pct=10, max_val=255),
+                 expected=expected_numeric_pct10 * 255, tolerance=1e-6)
+})
