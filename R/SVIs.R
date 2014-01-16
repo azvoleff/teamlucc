3e89a6dd8225bbd05afafa5b93d071d9ab1ed460
Alex Zvoleff <azvoleff@gmail.com>
2014-01-16 15:00:36 -0500

    Refactor SVIs code to use S4 classes.

--- R/SVIs.R
+++ R/SVIs.R
@@ -1,35 +1,64 @@
 #' Calculates the Normalized Difference Vegetation Index (NDVI)
 #'
+#' Calculates the NDVI, defined as: (nir - red) / (nir + red).
+#'
 #' @export
-#' @importFrom raster overlay
-#' @param red red \code{RasterLayer}
-#' @param nir near-infrared \code{RasterLayer}
-#' @param asinteger whether to round results to nearest integer
+#' @docType methods
+#' @rdname NDVI-methods
+#' @param red red
+#' @param nir near-infrared
 #' @param ... additional arguments as for \code{\link{writeRaster}}
 #' @examples
 #' NDVI_img <- NDVI(red=raster(L5TSR_1986, layer=3), nir=raster(L5TSR_1986, 
 #'                 layer=4))
 #' plot(NDVI_img)
-NDVI <- function(red, nir, asinteger=FALSE, ...) {
-    ret  <- overlay(red, nir, function(red, nir) {
-        res <- (nir - red) / (nir + red)
-        if(asinteger) {
-            res <- round(res)
-        }
-        return(res)
-    }, ...)
-    names(ret) <- 'NDVI'
-    return(ret)
+setGeneric("NDVI", function(red, nir, ...) {
+    standardGeneric("NDVI")
+})
+
+NDVI_calc <- function(red, nir) {
+    v <- (nir - red) / (nir + red)
+    return(v)
 }
+
+#' @rdname NDVI-methods
+#' @aliases NDVI,numeric,numeric,numeric-method
+setMethod("NDVI", signature(red="numeric", nir="numeric"),
+    function(red, nir) {
+        NDVI_calc(red, nir)
+    }
+)
+
+#' @rdname NDVI-methods
+#' @aliases NDVI,matrix,matrix,matrix-method
+setMethod("NDVI", signature(red="matrix", nir="matrix"),
+    function(red, nir) {
+        ret <- NDVI_calc(red, nir)
+        return(ret)
+    }
+)
+
+#' @rdname NDVI-methods
+#' @aliases NDVI,RasterLayer,RasterLayer,RasterLayer-method
+#' @importFrom raster overlay
+setMethod("NDVI", signature(red="RasterLayer", nir="RasterLayer"),
+    function(red, nir, ...) {
+        ret  <- overlay(red, nir, fun=function(red, nir) {
+                NDVI_calc(red, nir)
+            } , ...)
+        return(ret)
+    }
+)
 
 #' Calculates the Enhanced Vegetation Index (EVI)
 #'
 #' @export
+#' @docType methods
+#' @rdname EVI-methods
 #' @importFrom raster overlay
-#' @param blue blue \code{RasterLayer}
-#' @param red red \code{RasterLayer}
-#' @param nir near-infrared \code{RasterLayer}
-#' @param asinteger whether to round results to nearest integer
+#' @param blue blue
+#' @param red red
+#' @param nir near-infrared
 #' @param ... additional arguments as for \code{\link{writeRaster}}
 #' @references Huete, A. R., HuiQing Liu, and W. J. D. van Leeuwen. 1997. The
 #' use of vegetation indices in forested regions: issues of linearity and
@@ -40,17 +69,44 @@ NDVI <- function(red, nir, asinteger=FALSE, ...) {
 #' EVI_img <- EVI(blue=raster(L5TSR_1986, layer=1), red=raster(L5TSR_1986, layer=3), 
 #'                nir=raster(L5TSR_1986, layer=4))
 #' plot(EVI_img)
-EVI <- function(blue, red, nir, asinteger=FALSE, ...) {
-    ret <- overlay(blue, red, nir, function(blue, red, nir) {
-        res <- (2.5*(nir - red)) / (1 + nir + 6*red - 7.5*blue)
-        if(asinteger) {
-            res <- round(res)
-        }
-        return(res)
-    }, ...)
-    names(ret) <- 'EVI'
-    return(ret)
+setGeneric("EVI", function(blue, red, nir, ...) {
+    standardGeneric("EVI")
+})
+
+EVI_calc <- function(blue, red, nir) {
+    v <- (2.5*(nir - red)) / (1 + nir + 6*red - 7.5*blue)
+    return(v)
 }
+
+#' @rdname EVI-methods
+#' @aliases EVI,numeric,numeric,numeric-method
+setMethod("EVI", signature(blue="numeric", red="numeric", nir="numeric"),
+    function(blue, red, nir) {
+        EVI_calc(blue, red, nir)
+    }
+)
+
+#' @rdname EVI-methods
+#' @aliases EVI,numeric,numeric,numeric-method
+setMethod("EVI", signature(blue="matrix", red="matrix", nir="matrix"),
+    function(blue, red, nir) {
+        ret <- EVI_calc(blue, red, nir)
+        return(ret)
+    }
+)
+
+#' @rdname EVI-methods
+#' @aliases EVI,RasterLayer,RasterLayer,RasterLayer-method
+#' @importFrom raster overlay
+setMethod("EVI", signature(blue="RasterLayer", red="RasterLayer", 
+                           nir="RasterLayer"),
+    function(blue, red, nir, ...) {
+        ret <- overlay(blue, red, nir, fun=function(blue, red, nir) {
+                EVI_calc(blue, red, nir)
+            }, ...)
+        return(ret)
+    }
+)
 
 #' Calculates the Modified Soil-Adjusted Vegetation Index (MSAVI)
 #'
@@ -58,10 +114,11 @@ EVI <- function(blue, red, nir, asinteger=FALSE, ...) {
 #' MSAVI2 from Qi et al. (1994).
 #'
 #' @export
+#' @docType methods
+#' @rdname MSAVI2-methods
 #' @importFrom raster overlay
-#' @param red red \code{RasterLayer}
-#' @param nir near-infrared \code{RasterLayer}
-#' @param asinteger whether to round results to nearest integer
+#' @param red red
+#' @param nir near-infrared
 #' @param ... additional arguments as for \code{\link{writeRaster}}
 #' @references Qi, J., A. Chehbouni, A. R. Huete, Y. H. Kerr, and S.
 #' Sorooshian. 1994. A modified soil adjusted vegetation index. Remote Sensing
@@ -70,26 +127,53 @@ EVI <- function(blue, red, nir, asinteger=FALSE, ...) {
 #' MSAVI_img <- MSAVI2(red=raster(L5TSR_1986, layer=3),
 #'                     nir=raster(L5TSR_1986, layer=4))
 #' plot(MSAVI_img)
-MSAVI2 <- function(red, nir, asinteger=FALSE, ...) {
-    ret <- overlay(red, nir, function(red, nir) {
-        res <- (2*nir + 1 - sqrt((2*nir + 1)^2 - 8*(nir - red)))/2
-        if(asinteger) {
-            res <- round(res)
-        }
-        return(res)
-    }, ...)
-    names(ret) <- 'MSAVI2'
-    return(ret)
+setGeneric("MSAVI2", function(red, nir, ...) {
+    standardGeneric("MSAVI2")
+})
+
+MSAVI2_calc <- function(red, nir) {
+    v <- (2*nir + 1 - sqrt((2*nir + 1)^2 - 8*(nir - red)))/2
+    return(v)
 }
+
+#' @rdname MSAVI2-methods
+#' @aliases MSAVI2,numeric,numeric,numeric-method
+setMethod("MSAVI2", signature(red="numeric", nir="numeric"),
+    function(red, nir) {
+        MSAVI2_calc(red, nir)
+    }
+)
+
+#' @rdname MSAVI2-methods
+#' @aliases MSAVI2,matrix,matrix,matrix-method
+setMethod("MSAVI2", signature(red="matrix", nir="matrix"),
+    function(red, nir) {
+        ret <- MSAVI2_calc(red, nir)
+        return(ret)
+    }
+)
+
+#' @rdname MSAVI2-methods
+#' @aliases MSAVI2,RasterLayer,RasterLayer,RasterLayer-method
+#' @importFrom raster overlay
+setMethod("MSAVI2", signature(red="RasterLayer", nir="RasterLayer"),
+    function(red, nir, ...) {
+        ret  <- overlay(red, nir, fun=function(red, nir) {
+                MSAVI2_calc(red, nir)
+            } , ...)
+        return(ret)
+    }
+)
 
 #' Calculates the Atmospherically Resistant Vegetation Index (ARVI)
 #'
 #' @export
+#' @docType methods
+#' @rdname ARVI-methods
 #' @importFrom raster overlay
-#' @param blue blue \code{RasterLayer}
-#' @param red red \code{RasterLayer}
-#' @param nir near-infrared \code{RasterLayer}
-#' @param asinteger whether to round results to nearest integer
+#' @param blue blue
+#' @param red red
+#' @param nir near-infrared
 #' @param ... additional arguments as for \code{\link{writeRaster}}
 #' @references Kaufman, Y. J., and D. Tanre. 1996. Strategy for direct and
 #' indirect methods for correcting the aerosol effect on remote sensing: from
@@ -98,14 +182,42 @@ MSAVI2 <- function(red, nir, asinteger=FALSE, ...) {
 #' ARVI_img <- ARVI(blue=raster(L5TSR_1986, layer=1), red=raster(L5TSR_1986, 
 #'                  layer=3), nir=raster(L5TSR_1986, layer=4))
 #' plot(ARVI_img)
-ARVI <- function(blue, red, nir, asinteger=FALSE, ...) {
-    ret <- overlay(blue, red, nir, function(blue, red, nir) {
-        res <- (nir - 2*red - blue) / (nir + 2*red - blue)
-        if(asinteger) {
-            res <- round(res)
-        }
-        return(res)
-    }, ...)
-    names(ret) <- 'ARVI'
-    return(ret)
+setGeneric("ARVI", function(blue, red, nir, ...) {
+    standardGeneric("ARVI")
+})
+
+
+ARVI_calc <- function(blue, red, nir) {
+    v <- (nir - 2*red - blue) / (nir + 2*red - blue)
+    return(v)
 }
+
+#' @rdname ARVI-methods
+#' @aliases ARVI,numeric,numeric,numeric-method
+setMethod("ARVI", signature(blue="numeric", red="numeric", nir="numeric"),
+    function(blue, red, nir) {
+        ARVI_calc(blue, red, nir)
+    }
+)
+
+#' @rdname ARVI-methods
+#' @aliases ARVI,matrix,matrix,matrix-method
+setMethod("ARVI", signature(blue="matrix", red="matrix", nir="matrix"),
+    function(blue, red, nir) {
+        ret <- ARVI_calc(blue, red, nir)
+        return(ret)
+    }
+)
+
+#' @rdname ARVI-methods
+#' @aliases ARVI,RasterLayer,RasterLayer,RasterLayer-method
+#' @importFrom raster overlay
+setMethod("ARVI", signature(blue="RasterLayer", red="RasterLayer", 
+                            nir="RasterLayer"),
+    function(blue, red, nir, ...) {
+        ret <- overlay(blue, red, nir, fun=function(blue, red, nir) {
+                ARVI_calc(blue, red, nir)
+            }, ...)
+        return(ret)
+    }
+)
--- tests/testthat/test-SVIs.R
+++ tests/testthat/test-SVIs.R
@@ -1,21 +1,114 @@
-context("SVIs")
+context("SVIs - NDVI")
 
-test_that("NDVI equation is correct", {
-    expect_that(NDVI(red=10, nir=6), equals(c(NDVI=-.25)))
-    expect_that(NDVI(red=3, nir=7), equals(c(NDVI=.4)))
+NDVI_red_numeric <- c(10, 3)
+NDVI_nir_numeric <- c(6, 7)
+NDVI_expected_numeric <- c(-.25, .4)
+test_that("NDVI works for numeric", {
+    expect_that(NDVI(red=NDVI_red_numeric, nir=NDVI_nir_numeric), 
+                equals(NDVI_expected_numeric))
 })
 
-test_that("EVI equation is correct", {
-    expect_that(EVI(blue=1, red=5, nir=14), equals(c(EVI=.6)))
-    expect_that(EVI(blue=13, red=5, nir=4), equals(c(EVI=.04)))
+NDVI_red_matrix <- matrix(NDVI_red_numeric)
+NDVI_nir_matrix <- matrix(NDVI_nir_numeric)
+NDVI_expected_matrix <- matrix(NDVI_expected_numeric)
+test_that("NDVI from matrix works", {
+    expect_that(NDVI(red=NDVI_red_matrix, nir=NDVI_nir_matrix), 
+                equals(NDVI_expected_matrix))
 })
 
-test_that("MSAVI2 equation is correct", {
-    expect_that(MSAVI2(red=7, nir=3), equals(c(MSAVI2=-1)))
-    expect_that(MSAVI2(red=18, nir=5), equals(c(MSAVI2=-2)))
+NDVI_red_rast <- raster(NDVI_red_matrix)
+NDVI_nir_rast <- raster(NDVI_nir_matrix)
+NDVI_expected_rast <- raster(NDVI_expected_matrix)
+test_that("NDVI from raster works", {
+    expect_that(NDVI(red=NDVI_red_rast, nir=NDVI_nir_rast), 
+                equals(NDVI_expected_rast))
 })
 
-test_that("ARVI equation is correct", {
-    expect_that(ARVI(blue=1, red=5, nir=16), equals(c(ARVI=.2)))
-    expect_that(ARVI(blue=16, red=3, nir=4), equals(c(ARVI=3)))
+###############################################################################
+context("SVIs - EVI")
+
+EVI_blue_numeric <- c(1, 13)
+EVI_red_numeric <- c(5, 5)
+EVI_nir_numeric <- c(14, 4)
+EVI_expected_numeric <- c(.6, .04)
+test_that("EVI works for numeric", {
+    expect_that(EVI(blue=EVI_blue_numeric, red=EVI_red_numeric, 
+                    nir=EVI_nir_numeric), equals(EVI_expected_numeric))
 })
+
+EVI_blue_matrix <- matrix(EVI_blue_numeric)
+EVI_red_matrix <- matrix(EVI_red_numeric)
+EVI_nir_matrix <- matrix(EVI_nir_numeric)
+EVI_expected_matrix <- matrix(EVI_expected_numeric)
+test_that("EVI from matrix works", {
+    expect_that(EVI(blue=EVI_blue_matrix, red=EVI_red_matrix, 
+                    nir=EVI_nir_matrix), equals(EVI_expected_matrix))
+})
+
+EVI_blue_rast <- raster(EVI_blue_matrix)
+EVI_red_rast <- raster(EVI_red_matrix)
+EVI_nir_rast <- raster(EVI_nir_matrix)
+EVI_expected_rast <- raster(EVI_expected_matrix)
+test_that("EVI from raster works", {
+    expect_that(EVI(blue=EVI_blue_rast, red=EVI_red_rast, nir=EVI_nir_rast), 
+                equals(EVI_expected_rast))
+})
+
+
+###############################################################################
+context("SVIs - MSAVI2")
+
+MSAVI2_red_numeric <- c(7, 18)
+MSAVI2_nir_numeric <- c(3, 5)
+MSAVI2_expected_numeric <- c(-1, -2)
+test_that("MSAVI2 works for numeric", {
+    expect_that(MSAVI2(red=MSAVI2_red_numeric, nir=MSAVI2_nir_numeric), 
+                equals(MSAVI2_expected_numeric))
+})
+
+MSAVI2_red_matrix <- matrix(MSAVI2_red_numeric)
+MSAVI2_nir_matrix <- matrix(MSAVI2_nir_numeric)
+MSAVI2_expected_matrix <- matrix(MSAVI2_expected_numeric)
+test_that("MSAVI2 from matrix works", {
+    expect_that(MSAVI2(red=MSAVI2_red_matrix, nir=MSAVI2_nir_matrix), 
+                equals(MSAVI2_expected_matrix))
+})
+
+MSAVI2_red_rast <- raster(MSAVI2_red_matrix)
+MSAVI2_nir_rast <- raster(MSAVI2_nir_matrix)
+MSAVI2_expected_rast <- raster(MSAVI2_expected_matrix)
+test_that("MSAVI2 from raster works", {
+    expect_that(MSAVI2(red=MSAVI2_red_rast, nir=MSAVI2_nir_rast), 
+                equals(MSAVI2_expected_rast))
+})
+
+###############################################################################
+context("SVIs - ARVI")
+
+ARVI_blue_numeric <- c(1, 16)
+ARVI_red_numeric <- c(5, 3)
+ARVI_nir_numeric <- c(16, 4)
+ARVI_expected_numeric <- c(.2, 3)
+test_that("ARVI works for numeric", {
+    expect_that(ARVI(blue=ARVI_blue_numeric, red=ARVI_red_numeric, 
+                     nir=ARVI_nir_numeric), equals(ARVI_expected_numeric))
+})
+
+ARVI_blue_matrix <- matrix(ARVI_blue_numeric)
+ARVI_red_matrix <- matrix(ARVI_red_numeric)
+ARVI_nir_matrix <- matrix(ARVI_nir_numeric)
+ARVI_expected_matrix <- matrix(ARVI_expected_numeric)
+test_that("ARVI from matrix works", {
+    expect_that(ARVI(blue=ARVI_blue_matrix, red=ARVI_red_matrix, 
+                     nir=ARVI_nir_matrix), equals(ARVI_expected_matrix))
+})
+
+ARVI_blue_rast <- raster(ARVI_blue_matrix)
+ARVI_red_rast <- raster(ARVI_red_matrix)
+ARVI_nir_rast <- raster(ARVI_nir_matrix)
+ARVI_expected_rast <- raster(ARVI_expected_matrix)
+test_that("ARVI from raster works", {
+    expect_that(ARVI(blue=ARVI_blue_rast, red=ARVI_red_rast, 
+                     nir=ARVI_nir_rast), equals(ARVI_expected_rast))
+})
+
