e1559d019516726c5712537aca18d9a901c790e6
Alex Zvoleff <azvoleff@gmail.com>
2014-01-22 16:32:36 -0500

    Fix parallel scaling.

--- R/scale_raster.R
+++ R/scale_raster.R
@@ -68,21 +68,26 @@ scale_stack_or_brick <- function(x, power_of, max_out, round_output, do_scaling)
         if (do_scaling) {
             scale_outputs <- foreach(unscaled_layer=unstack(x), .combine='addLayer', 
                                 .multicombine=TRUE, .init=raster(), 
-                                .packages=c('raster', 'rgdal', 'teamr')) %dopar% {
-                scale_output <- scale_layer(unscaled_layer, power_of, max_out, round_output, do_scaling)
+                                .packages=c('raster', 'rgdal', 'teamr'),
+                                .export=c('scale_layer')) %dopar% {
+                scale_output <- scale_layer(unscaled_layer, power_of, max_out, 
+                                            round_output, do_scaling)
             }
         } else {
             scale_factors <- foreach(unscaled_layer=unstack(x), 
                                 .multicombine=TRUE, .init=raster(), 
-                                .packages=c('raster', 'rgdal', 'teamr')) %dopar% {
-                scale_factor <- scale_layer(unscaled_layer, power_of, max_out, round_output, do_scaling)
+                                .packages=c('raster', 'rgdal', 'teamr'),
+                                .export=c('scale_layer')) %dopar% {
+                scale_factor <- scale_layer(unscaled_layer, power_of, max_out, 
+                                            round_output, do_scaling)
             }
         }
     } else {
         scale_outputs <- c()
         for (layer_num in 1:nlayers(x)) {
             unscaled_layer <- raster(x, layer=layer_num)
-            scale_output <- scale_layer(unscaled_layer, power_of, max_out, round_output, do_scaling)
+            scale_output <- scale_layer(unscaled_layer, power_of, max_out, 
+                                        round_output, do_scaling)
             scale_outputs <- c(scale_outputs, list(scale_output))
         }
         if (do_scaling) {
