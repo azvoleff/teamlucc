5b7344f3b15b7a4272731c0c23b1113eda88826c
Alex Zvoleff <azvoleff@gmail.com>
2014-02-05 13:17:16 -0500

    Add gap fill stubs.

---
+++ R/team_fill_clouds.R
@@ -0,0 +1,11 @@
+#' Perform heavy cloud filling
+#'
+#' Calls CLOUD_REMOVE.pro IDL script by Zhu Xiaolin to fill heavy clouds in 
+#' Landsat image.
+#'
+#' @export
+#' @references Zhu, X., Gao, F., Liu, D., Chen, J., 2012. A modified 
+#' neighborhood similar pixel interpolator approach for removing thick clouds 
+#' in Landsat images. Geoscience and Remote Sensing Letters, IEEE 9, 521-525.
+team_fill_clouds <- function() {
+}
---
+++ R/team_fill_gaps.R
@@ -0,0 +1,11 @@
+#' Perform SLC-off gap fill of Landsat 7 ETM+ image
+#'
+#' Calls GNSPI.pro IDL script by Zhu Xiaolin to fill gaps in SLC-off Landsat 7 
+#' ETM+ image.
+#'
+#' @export
+#' @references Zhu, X., Liu, D., Chen, J., 2012. A new geostatistical approach 
+#' for filling gaps in Landsat ETM+ SLC-off images. Remote Sensing of 
+#' Environment 124, 49-60.
+team_fill_gaps <- function() {
+}
