c552fb51759c12bd12dcf033e6bf5e8aef721d72
Alex Zvoleff <azvoleff@gmail.com>
2014-02-05 13:19:34 -0500

    Fill out team_chg_detect function.

--- R/team_chg_detect.R
+++ R/team_chg_detect.R
@@ -4,32 +4,60 @@
 #' function.
 #'
 #' @export
-#' @param t1_predictions output from \code{team_classify_image} for time 1 
-#' image
-#' @param t2_predictions output from \code{team_classify_image} for time 2 
-#' image
+#' @param t1_classes cover classes as output from \code{team_classify_image} 
+#' for time 1 image
+#' @param t1_probs per class probabilities as output from 
+#' \code{team_classify_image} for time 1 image
+#' @param t2_probs per class probabilities as output from 
+#' \code{team_classify_image} for time 2 image
+#' @param output_path the path to use for the output
+#' @param n_cpus the number of CPUs to use for processes that can run in 
+#' parallel
+#' @param overwrite whether to overwrite existing files (otherwise an error 
+#' will be raised)
 #' @param notify notifier to use (defaults to \code{print} function). See the 
 #' \code{notifyR} package for one way of sending notifications from R. The 
 #' \code{notify} function should accept a string as the only argument.
-team_chg_detect <- function(t1_predictions, t2_predictions, notify) {
-    timer <- Track_time(notify)
+team_chg_detect <- function(t1_classes, t1_probs, t2_probs, output_basename, 
+                            output_path, n_cpus=1, overwrite=FALSE, 
+                            notify=print) {
+    if (!file_test("-d", output_path)) {
+        stop(paste(output_path, "does not exist"))
+    }
 
+    timer <- Track_time(notify)
     timer <- start_timer(timer, label='Change detection')
+
+    if (n_cpus > 1) beginCluster(n_cpus)
 
     ################################################################################
     # Calculate change magnitude and direction
     ################################################################################
     timer <- start_timer(timer, label='Change magnitude and direction')
-    t0_probs <- brick('L5TSR_1986_probs.grd')
-    t1_probs <- brick('L5TSR_2001_probs.grd')
-    chg_dir_image <- chg_dir(t0_probs, t1_probs, filename='L5TSR_1986_to_2001_chgdir')
-    chg_mag_image <- chg_mag(t0_probs, t1_probs, filename='L5TSR_1986_to_2001_chgmag')
+    chg_dir_filename <- filename=file.path(output_path, 
+                                           paste(output_basename, 
+                                                 'chg_dir.envi', sep='_'))
+    chg_dir_image <- chg_dir(t0_probs, t1_probs, filename=chg_dir_filename, 
+                             overwrite=overwrite)
+    chg_mag_filename <- filename=file.path(output_path, 
+                                           paste(output_basename, 
+                                                 'chg_mag.envi', sep='_'))
+    chg_mag_image <- chg_mag(t0_probs, t1_probs, filename=chg_mag_filename, 
+                             overwrite=overwrite)
     timer <- stop_timer(timer, label='Change magnitude and direction')
 
     ################################################################################
     # Calculate change trajectories
     ################################################################################
     timer <- start_timer(timer, label='Change trajectories')
+    chg_traj_filename <- filename=file.path(output_path, 
+                                           paste(output_basename, 
+                                                 'chg_traj.envi', sep='_'))
+    #TODO: Determine threshold with DFPS or automatic algorithm
+    chg_traj_traj <- chg_traj(t1_classes, chg_mag_image, chg_dir_image, 
+                              classnames=levels(t1_classes),
+                              threshold=threshold, overwrite=overwrite, 
+                              filename=chg_traj_filename)
     timer <- stop_timer(timer, label='Change trajectories')
 
     timer <- stop_timer(timer, label='Change detection')
