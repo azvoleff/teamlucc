0874c2a22cb769757642c266d1eb09ac30049998
Alex Zvoleff <azvoleff@gmail.com>
2014-02-06 12:53:19 -0500

    Add min_pixels parameter.

--- R/fill_clouds.R
+++ R/fill_clouds.R
@@ -36,6 +36,7 @@ format_IDL_param <- function(varname, varvalue) {
 #' be automatically assigned based on \code{img_clear}
 #' @param script_path the path to the CLOUD_REMOVE.sav file
 #' @param num_class set the estimated number of classes in image
+#' @param min_pixel the sample size of similar pixels
 #' @param extent1 set the range of cloud neighborhood
 #' @param DN_min minimum of DN
 #' @param DN_max maximum of DN
@@ -45,21 +46,24 @@ format_IDL_param <- function(varname, varvalue) {
 #' neighborhood similar pixel interpolator approach for removing thick clouds 
 #' in Landsat images. Geoscience and Remote Sensing Letters, IEEE 9, 521-525.
 fill_clouds <- function(img_cloudy, img_clear, img_cloud_mask, out_name=NULL,
-                        script_path='CLOUD_REMOVE.sav', num_class=1, extent1=1, 
-                        DN_min=0, DN_max=255, patch_long=1000,
+                        script_path='CLOUD_REMOVE.sav', num_class=1, 
+                        min_pixel=20, extent1=1, DN_min=0, DN_max=255, 
+                        patch_long=1000,
                         idl="C:/Program Files/Exelis/IDL83/bin/bin.x86_64/idl.exe") {
-    # img_cloudy <- 'C:/Users/azvoleff/Code/TEAM/team_IDL_code/cloud_removal/test data/L20080724_cloudy'
-    # img_clear <- 'C:/Users/azvoleff/Code/TEAM/team_IDL_code/cloud_removal/test data/L20080606'
-    # img_cloud_mask <- 'C:/Users/azvoleff/Code/TEAM/team_IDL_code/cloud_removal/test data/cloud_mask'
-    # script_path <- 'C:/Users/azvoleff/Code/TEAM/team_IDL_code/cloud_removal/CLOUD_REMOVE.pro'
-    # out_name=NULL
-    # num_class <- 1
-    # extent1 <- 1
-    # DN_min <- 0
-    # DN_max <- 255
-    # patch_long <- 1000
-    # idl <- "C:/Program Files/Exelis/IDL83/bin/bin.x86_64/idl.exe"
-    #
+    img_cloudy <- 'C:/Users/azvoleff/Code/TEAM/team_IDL_code/cloud_removal/test data/L20080724_cloudy'
+    img_clear <- 'C:/Users/azvoleff/Code/TEAM/team_IDL_code/cloud_removal/test data/L20080606'
+    img_cloud_mask <- 'C:/Users/azvoleff/Code/TEAM/team_IDL_code/cloud_removal/test data/cloud_mask'
+    script_path <- 'C:/Users/azvoleff/Code/TEAM/team_IDL_code/cloud_removal/CLOUD_REMOVE.pro'
+    script_path <- 'C:/Users/azvoleff/Code/TEAM/team_IDL_code/cloud_removal/CLOUD_REMOVE_FAST.pro'
+    out_name=NULL
+    num_class <- 1
+    min_pixel <- 20
+    extent1 <- 1
+    DN_min <- 0
+    DN_max <- 255
+    patch_long <- 1000
+    idl <- "C:/Program Files/Exelis/IDL83/bin/bin.x86_64/idl.exe"
+    
     if (!(file_test('-x', idl) || file_test('-f', idl))) {
         stop('IDL not found - check "idl" parameter')
     }
@@ -81,9 +85,11 @@ fill_clouds <- function(img_cloudy, img_clear, img_cloud_mask, out_name=NULL,
     }
 
     param_names <- c("cloudy_file", "clear_file", "mask_file", "out_name", 
-                  "num_class", "extent1", "DN_min", "DN_max", "patch_long")
+                     "num_class", "min_pixel", "extent1", "DN_min", "DN_max", 
+                     "patch_long")
     param_vals <- list(img_cloudy, img_clear, img_cloud_mask, out_name, 
-                      num_class, extent1, DN_min, DN_max, patch_long)
+                       num_class, min_pixel, extent1, DN_min, DN_max, 
+                       patch_long)
     idl_params <- mapply(format_IDL_param, param_names, param_vals)
     idl_params <- paste(idl_params, collapse='')
 
