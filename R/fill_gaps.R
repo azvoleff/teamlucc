73ede68888599192d50080427e3cce08cd51b0ef
Alex Zvoleff <azvoleff@gmail.com>
2014-02-06 17:32:25 -0500

    Revert "Reformat for included IDL scripts." Added a new brach for IDL VM development.
    This reverts commit 19fedd94797035123919c4293ef62b308962c10c.

--- R/fill_gaps.R
+++ R/fill_gaps.R
@@ -1,13 +1,14 @@
 #' Perform SLC-off gap fill of Landsat 7 ETM+ image
 #'
-#' Calls GNSPI.pro IDL script by Xiaolin Zhu to fill gaps in SLC-off Landsat 7 
-#' ETM+ image.
+#' Calls GNSPI.pro IDL script by Zhu Xiaolin to fill gaps in SLC-off Landsat 7 
+#' ETM+ image. The GNSPI script should be provided as a .sav file.
 #'
 #' @importFrom tools file_path_sans_ext
 #' @param slc_off_file the SLC-off Landsat 7 file to gap fill
 #' @param input_file the first file to use to fill in the gaps
 #' @param timeseries_files a timeseries of files to use as additional inputs to 
 #' the gap fill algorithm
+#' @param script_path the path to the GNSPI.sav file
 #' @param out_base path and base filename for the output file. The script will 
 #' save the output files by appending "_GNSPI.envi" and 
 #' "_GNSPI_uncertainty.envi" to this base filename.
@@ -23,13 +24,29 @@
 #' for filling gaps in Landsat ETM+ SLC-off images. Remote Sensing of 
 #' Environment 124, 49-60.
 fill_gaps <- function(slc_off_file, input_file, timeseries_files, 
-                      out_base=NULL, sample_size=20, size_wind=12, class_num=4, 
-                      DN_min=0.0, DN_max=1.0, patch_long=1000,
+                      script_path='GNSPI.sav', out_base=NULL, sample_size=20, 
+                      size_wind=12, class_num=4, DN_min=0.0, DN_max=1.0, 
+                      patch_long=1000,
                       idl="C:/Program Files/Exelis/IDL83/bin/bin.x86_64/idl.exe") {
-    script_path <- system.file("inst/idl", "GNSPI.pro", package="teamr")
+
+    slc_off_file <- 'C:/Users/azvoleff/Code/TEAM/team_IDL_code/gap_fill/test data/TM20100429_toaR_gap'
+    input_file <- 'C:/Users/azvoleff/Code/TEAM/team_IDL_code/gap_fill/test data/TM20100208_toaR'
+    timeseries_files <- list('C:/Users/azvoleff/Code/TEAM/team_IDL_code/gap_fill/test data/TM20100515_toaR')
+    script_path <- 'C:/Users/azvoleff/Code/TEAM/team_IDL_code/gap_fill/GNSPI.pro'
+    out_base <- 'C:/Users/azvoleff/Code/TEAM/team_IDL_code/gap_fill/test data/TM20100429_toaR'
+    sample_size <- 20
+    size_wind <- 12
+    class_num <- 4
+    DN_min <- 0.0
+    DN_max <- 1.0
+    patch_long <- 1000
+    idl <- "C:/Program Files/Exelis/IDL83/bin/bin.x86_64/idl.exe"
 
     if (!(file_test('-x', idl) || file_test('-f', idl))) {
         stop('IDL not found - check "idl" parameter')
+    }
+    if (!(file_test('-f', script_path))) {
+        stop('GNSPI.sav not found - check script_path parameter')
     }
     if (!(file_test('-f', slc_off_file))) {
         stop('SLC-off file not found- check slc_off_file parameter')
