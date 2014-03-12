c7986e3d99d0b589d04ebc8400d6efda634f4704
Alex Zvoleff <azvoleff@gmail.com>
2014-03-12 07:28:43 -0400

    Fix travis error.

--- tests/testthat/test-cloud_remove.R
+++ tests/testthat/test-cloud_remove.R
@@ -6,6 +6,6 @@ context("cloud_remove")
 # Test that areas that ARE cloud masked have reflectance that do not vary 
 # largely from the clear reflectance
 
-plotRGB(cloudy, 3, 2, 1)
-plotRGB(clear, 3, 2, 1)
-plotRGB(clout_filled, 3, 2, 1)
+# plotRGB(cloudy, 3, 2, 1)
+# plotRGB(clear, 3, 2, 1)
+# plotRGB(clout_filled, 3, 2, 1)
