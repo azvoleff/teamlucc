fcbb888851096481d155c0ae51a8002e0c7b0ebf
Alex Zvoleff <azvoleff@gmail.com>
2014-05-15 22:10:22 -0400

    Fix for roxygen update.

--- R/utm_zone.R
+++ R/utm_zone.R
@@ -5,10 +5,9 @@
 #'
 #' Based on the code on gis.stackexchange.com at http://bit.ly/17SdcuN.
 #'
-#' @export
+#' @export utm_zone
 #' @docType methods
 #' @import methods
-#' @rdname utm_zone-methods
 #' @param x a longitude (with western hemisphere longitudes negative), or a 
 #' \code{Spatial} object
 #' @param y a latitude (with southern hemisphere latitudes negative), or 
@@ -67,7 +66,6 @@ utm_zone_calc <- function(x, y, proj4string) {
     }
 }
 
-#' @rdname utm_zone-methods
 #' @aliases utm_zone,numeric,numeric,logical-method
 setMethod("utm_zone", signature("numeric", "numeric"),
     function(x, y, proj4string) {
@@ -75,7 +73,6 @@ setMethod("utm_zone", signature("numeric", "numeric"),
     }
 )
 
-#' @rdname utm_zone-methods
 #' @importFrom rgeos gCentroid
 #' @importFrom sp Spatial coordinates spTransform
 #' @aliases utm_zone,Spatial,missing,logical-method
