bb4a14b8c0d08bdd9ace146b681d16936575b93a
Alex Zvoleff <azvoleff@gmail.com>
2014-06-08 17:21:30 -0400

    Fix for case of pixels with no identified seasons.

--- R/tpa2df.R
+++ R/tpa2df.R
@@ -3,19 +3,24 @@
 #' @export
 #' @param x A string giving the location of a .tpa file output by 
 #' TIMESAT
+#' @param max_num_seasons the maximum number of seasons for any of the pixels 
+#' in the file
 #' @return A data.frame containing 14 columns: row, col, season, start, end, 
 #' length, base_value, peak_time, peak_value, amp, left_deriv, right_deriv, 
 #' large_integ, and small_integ
 #' @examples
 #' # TODO: Need to add examples here, and need to include a sample TIMESAT tpa 
 #' # file in the package data.
-tpa2df <- function(x) {
+tpa2df <- function(x, max_num_seasons) {
     if (missing(x) || !grepl('[.]tpa$', tolower(x))) {
         stop('must specify a .tpa file')
     }
+    if (missing(max_num_seasons) || max_num_seasons < 1) {
+        stop('must specify maximum number of seasons represented in tpa file')
+    }
 
     # The number of seasonal indicators output by TIMESAT.
-    SEASONAL_INDICATORS <- 11
+    NUM_SEASONAL_INDICATORS <- 11
 
     # Number of elements in the tpa file line header (which are normally: row, 
     # column, number of seasons).
@@ -44,34 +49,40 @@ tpa2df <- function(x) {
     colstart <- file_header[5]
     colstop <- file_header[6]
 
-    num_pixels <- (colstop - colstart) * (rowstop - rowstart)
+    num_rows <- rowstop - rowstart + 1
+    num_cols <- colstop - colstart + 1
 
-    # Setup tpa_data matrix to hold the data. Need to read first line of file 
-    # to get number of seasons in order to set data matrix dimensions.
-    first_line_header <- offset_readBin(raw_vector, integer(), n=3, size=4, increment_offset=FALSE)
-    num_seasons <- first_line_header[3]
-    tpa_data <- matrix(nrow=num_pixels*num_seasons,
-                       ncol=(LINE_HEADER_SIZE + SEASONAL_INDICATORS))
+    num_pixels <- num_cols * num_rows
+
+    tpa_data <- matrix(nrow=num_pixels*max_num_seasons,
+                       ncol=(LINE_HEADER_SIZE + NUM_SEASONAL_INDICATORS))
 
     # Read the data and save it in the tpa_data matrix
     for (pixelnum in 1:num_pixels) {
         line_header <- offset_readBin(raw_vector, integer(), n=3, size=4)
         # Line header format is: rownum colnum numseasons
-        if (line_header[3] != num_seasons) {
-            stop(paste('pixel', pixelnum, 'has', line_header[3],
-                       'seasons, but first line of .tpa file had', num_seasons, 
+        num_seasons <- line_header[3]
+        if (num_seasons > max_num_seasons) {
+            stop(paste('pixel', pixelnum, 'has', num_seasons,
+                       'seasons, but max_num_seasons was set to ', max_num_seasons, 
                        'seasons'))
         }
-        for (seasonnum in 1:num_seasons) {
+        if (num_seasons == 0) {
+            # No seasons identified for this pixel - skip
+            next
+        }
+        for (seasonnum in 1:max_num_seasons) {
+            # Seasons were found for this pixel - read them
+            tpa_data_row <- (pixelnum - 1)*num_seasons + seasonnum
             line_data <- offset_readBin(raw_vector, numeric(), 
-                                        n=SEASONAL_INDICATORS, size=4)
-            tpa_data_row <- (pixelnum-1)*num_seasons + seasonnum
-            tpa_data[tpa_data_row, ] <- c(line_header[1], line_header[2],
+                                        n=NUM_SEASONAL_INDICATORS, size=4)
+            tpa_data[tpa_data_row, ] <- c(line_header[1], line_header[2], 
                                           seasonnum, line_data)
         }
     }
 
     tpa_data <- data.frame(tpa_data)
+    tpa_data <- tpa_data[!(rowSums(is.na(tpa_data)) == ncol(tpa_data)), ]
     names(tpa_data) <- c("row", "col", "season", "start", "end", "length",
                          "base_value", "peak_time", "peak_value", "amp", "left_deriv",
                          "right_deriv", "large_integ", "small_integ")
