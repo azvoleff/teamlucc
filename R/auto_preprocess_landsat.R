9c118b8948fb5f0204e33cfc3241d7cbf6366f31
Alex Zvoleff <azvoleff@gmail.com>
2014-06-18 15:34:09 -0400

    Use gdal VRTs to avoid need for unstack_ledapscdr

--- R/auto_preprocess_landsat.R
+++ R/auto_preprocess_landsat.R
@@ -1,3 +1,10 @@
+get_gdalinfo_item <- function(item, gdalinfo_text) {
+    gdalinfo_text <- gdalinfo_text[grepl(item, gdalinfo_text)]
+    if (length(gdalinfo_text) > 1) stop('more than one item found')
+    gdalinfo_text <- gsub(paste0('[ ]*', item, '='), '', gdalinfo_text)
+    return(gdalinfo_text)
+}
+
 calc_cloud_mask <- function(mask_stack, mask_type, ...) {
     if (mask_type == 'fmask') {
         # Make a mask where clouds and gaps are coded as 1, clear as 0
@@ -50,12 +57,29 @@ calc_cloud_mask <- function(mask_stack, mask_type, ...) {
 #' correction.
 #'
 #' \code{mask_type} chooses the cloud mask to use if topographic correction is 
-#' performed (\code{tc=TRUE}). TODO: Fill in coding of cloud masks.
+#' performed (\code{tc=TRUE}). The mask can be one of three different options: 
+#' "6S", "fmask", or "combined". Each option uses a different combination of 
+#' cloud mask layers from the CDR product. The "6S" masks out any areas coded 
+#' as fill (fill_QA=255), cloud (cloud_QA=255), cloud shadow
+#' (cloud_shadow_QA=255) or adjacent to cloud (adjacent_cloud_QA=255). The 
+#' "fmask" option masks out any areas coded as fill (fmask=255), cloud 
+#' (fmask=4) or cloud shadow (fmask=2).  The combined option combines the "6S" 
+#' and "fmask" approaches to masks out areas coded as fill, cloud, cloud 
+#' shadow, or adjacent to cloud using either method.
+#'
+#' \tabular{6S}{
+#'     Nodata               \tab 0 \cr
+#'     Forest               \tab 1 \cr
+#'     Non-forest           \tab 2 \cr
+#'     Forest loss          \tab 3 \cr
+#'     Forest gain          \tab 4 \cr
+#'     Forest loss and gain \tab 5 \cr
+#'     Water                \tab 6 \cr
+#' }
+#'
 #'
 #' Prior to running \code{auto_preprocess_landsat}, \code{\link{espa_extract}} 
-#' should be used to extract the original zipfiles supplied by USGS.  
-#' \code{\link{unstack_ledapscdr}} should then be used to unstack the HDF 
-#' format files and convert them into ENVI binary format. To perform 
+#' should be used to extract the original zipfiles supplied by USGS. To perform 
 #' topographic correction with \code{auto_preprocess_landsat}, first run 
 #' \code{\link{auto_setup_dem}} to preprocess a set of DEM tiles. Then run 
 #' \code{auto_preprocess_landsat} with the \code{tc=TRUE} option.
@@ -63,10 +87,10 @@ calc_cloud_mask <- function(mask_stack, mask_type, ...) {
 #' @importFrom rgeos gIntersection
 #' @importFrom wrspathrow pathrow_poly
 #' @importFrom tools file_path_sans_ext
-#' @importFrom gdalUtils gdalwarp
+#' @importFrom gdalUtils gdalwarp get_subdatasets gdalinfo gdalbuildvrt
 #' @importFrom sp is.projected
-#' @param image_dirs list of paths to a set of Landsat CDR image files in ENVI 
-#' format as output by the \code{unstack_ledapscdr} function.
+#' @param image_dirs list of paths to a set of Landsat CDR image files in HDF 
+#' format
 #' @param prefix string to use as a prefix for all filenames
 #' @param tc whether to topographically correct imagery (if \code{TRUE}, then 
 #' \code{dem_path} must be specified)
@@ -102,6 +126,9 @@ auto_preprocess_landsat <- function(image_dirs, prefix, tc=FALSE,
                                     mask_type='fmask', mask_output=FALSE, 
                                     n_cpus=1, cleartmp=FALSE,  overwrite=FALSE, 
                                     notify=print, verbose=FALSE) {
+    if (tc && is.null(dem_path)) {
+        stop('dem_path must be supplied if tc=TRUE')
+    }
     if (tc && !file_test("-d", dem_path)) {
         stop(paste(dem_path, "does not exist"))
     }
@@ -119,80 +146,33 @@ auto_preprocess_landsat <- function(image_dirs, prefix, tc=FALSE,
     timer <- Track_time(notify)
 
     # Setup a regex to identify Landsat CDR images
-    lndsr_regex <- '^lndsr.((LT4)|(LT5)|(LE7)|(LE8))[0-9]{6}[12][0-9]{6}[a-zA-Z]{3}[0-9]{2}'
+    lndsr_regex <- '^lndsr.((LT4)|(LT5)|(LE7)|(LE8))[0-9]{6}[12][0-9]{6}[a-zA-Z]{3}[0-9]{2}.hdf$'
 
     image_bands <- c('band1', 'band2', 'band3', 'band4', 'band5', 'band7')
     mask_bands <- c('fill_QA', 'fmask_band', 'cloud_QA', 'cloud_shadow_QA', 
                     'adjacent_cloud_QA')
-    image_files <- c()
-    image_stacks <- c()
-    mask_stacks <- c()
+
+    lndsr_files <- c()
     for (image_dir in image_dirs) {
         if (!file_test("-d", image_dir)) {
             stop(paste(image_dir, "does not exist"))
         }
-        lndsr_files <- dir(image_dir, pattern=lndsr_regex)
-        loc <- regexpr(lndsr_regex, lndsr_files)
-        stop_char <- loc + attr(loc, 'match.length') - 1
-        image_basenames <- unique(substr(lndsr_files, loc, stop_char))
-
-        if (length(image_basenames) == 0) {
-            stop(paste('no files found in', image_dir))
-        }
-
-        for (image_basename in image_basenames) {
-            band_files <- c()
-            for (image_band in image_bands) {
-                band_files <- c(band_files,
-                                paste(file.path(image_dir, image_basename), 
-                                      image_band, sep='_'))
-            }
-            band_files <- paste0(band_files, '.envi')
-            image_stack <- stack(band_files)
-            names(image_stack) <- image_bands
-            image_files <- c(image_files, list(band_files))
-            # Read gain and offset from the metadata file as raster does not 
-            # support passing as.is=TRUE to readGDAL, meaning that GDAL 
-            # automatically scales the returned data turning it into floats.
-            gain(image_stack) <- 1/as.numeric(get_metadata_item(band_files[1], 
-                                                                'scale_factor'))
-            offs(image_stack) <- as.numeric(get_metadata_item(band_files[1], 
-                                                              'add_offset'))
-
-            mask_band_files <- c()
-            for (mask_band in mask_bands) {
-                mask_band_files <- c(mask_band_files,
-                                     paste(file.path(image_dir, 
-                                                     image_basename), 
-                                           mask_band, sep='_'))
-            }
-            mask_band_files <- paste0(mask_band_files, '.envi')
-            mask_stack <- stack(mask_band_files)
-            names(mask_stack) <- mask_bands
-
-            image_stacks <- c(image_stacks, image_stack)
-            mask_stacks <- c(mask_stacks, mask_stack)
-        }
+        lndsr_files <- c(lndsr_files, dir(image_dir, pattern=lndsr_regex, full.names=TRUE))
     }
 
-    for (n in 1:length(image_stacks)) {
-        image_stack <- image_stacks[[n]]
-        mask_stack <- mask_stacks[[n]]
-        band1_imagefile <- image_files[[n]][1]
+    if (length(lndsr_files) == 0) {
+        stop('no Landsat CDR HDF files found')
+    }
 
-        if (is.null(output_path)) {
-            this_output_path <- dirname(band1_imagefile)
-        } else {
-            this_output_path  <- output_path
-        }
-
+    for (lndsr_file in lndsr_files) {
         ######################################################################
         # Determine image basename for use in naming subsequent files
-        aq_date <- get_metadata_item(band1_imagefile, 'AcquisitionDate')
+        lndsr_file_gdalinfo <- gdalinfo(lndsr_file)
+        aq_date <- get_gdalinfo_item('AcquisitionDate', lndsr_file_gdalinfo)
         aq_date <- strptime(aq_date, format="%Y-%m-%dT%H:%M:%OSZ")
-        short_name  <- get_metadata_item(band1_imagefile, 'ShortName')
-        WRS_Path <- sprintf('%03i', as.numeric(get_metadata_item(band1_imagefile, 'WRS_Path')))
-        WRS_Row <- sprintf('%03i', as.numeric(get_metadata_item(band1_imagefile, 'WRS_Row')))
+        short_name  <- get_gdalinfo_item('ShortName', lndsr_file_gdalinfo)
+        WRS_Path <- sprintf('%03i', as.numeric(get_gdalinfo_item('WRS_Path', lndsr_file_gdalinfo)))
+        WRS_Row <- sprintf('%03i', as.numeric(get_gdalinfo_item('WRS_Row', lndsr_file_gdalinfo)))
         image_basename <- paste0(WRS_Path, '-', WRS_Row, '_',
                                  format(aq_date, '%Y-%j'), '_', short_name)
 
@@ -204,6 +184,22 @@ auto_preprocess_landsat <- function(image_dirs, prefix, tc=FALSE,
         # supplied), or the UTM zone of the centroid of this path and row.
         if (verbose) timer <- start_timer(timer, label=paste(image_basename, 
                                                              '-', 'cropping and reprojecting'))
+
+        sds <- get_subdatasets(lndsr_file)
+
+        band_sds <- sds[grepl(paste0(':(', paste(image_bands, collapse='|'), ')$'), sds)]
+        band_vrt_file <- extension(rasterTmpFile(), '.vrt')
+        gdalbuildvrt(band_sds, band_vrt_file, separate=TRUE)
+
+        mask_sds <- sds[grepl(paste0(':(', paste(mask_bands, collapse='|'), ')$'), sds)]
+        mask_vrt_file <- extension(rasterTmpFile(), '.vrt')
+        gdalbuildvrt(mask_sds, mask_vrt_file, separate=TRUE, srcnodata='None')
+
+        if (is.null(output_path)) {
+            this_output_path <- dirname(lndsr_file)
+        } else {
+            this_output_path  <- output_path
+        }
 
         this_pathrow_poly <- pathrow_poly(as.numeric(WRS_Path), 
                                           as.numeric(WRS_Row))
@@ -225,32 +221,30 @@ auto_preprocess_landsat <- function(image_dirs, prefix, tc=FALSE,
             crop_area <- this_pathrow_poly
         }
         out_te <- as.numeric(bbox(crop_area))
-
         to_res <- c(30, 30)
-        image_stack_temp_file <- extension(rasterTmpFile(), '.envi')
-        # Ensure image layers are written to desk in a single raster file
-        image_stack <- writeRaster(image_stack, filename=image_stack_temp_file,
-                                   datatype='INT2S')
-        image_stack_reproj_file <- extension(rasterTmpFile(), '.envi')
-        image_stack <- gdalwarp(image_stack_temp_file,
+
+        image_stack_reproj_file <- extension(rasterTmpFile(), '.tif')
+        image_stack <- gdalwarp(band_vrt_file,
                                 dstfile=image_stack_reproj_file,
                                 te=out_te, t_srs=to_srs, tr=to_res, 
-                                r='cubicspline', output_Raster=TRUE, of="ENVI", 
+                                r='cubicspline', output_Raster=TRUE, of="GTiff", 
                                 multi=TRUE, wo=paste0("NUM_THREADS=", n_cpus), 
                                 overwrite=overwrite)
-        names(image_stack) <- image_bands
-        # Ensure mask layers are written to desk in a single raster file
-        mask_stack_temp_file <- extension(rasterTmpFile(), '.envi')
-        mask_stack <- writeRaster(mask_stack, filename=mask_stack_temp_file,
-                                  datatype='INT2S')
-        mask_stack_reproj_file <- extension(rasterTmpFile(), '.envi')
-        mask_stack <- gdalwarp(mask_stack_temp_file,
+        # Can't just directly assign image_bands as the names since the bands 
+        # may have been read in different order from the HDF file
+        names(image_stack) <- gsub('^:', '', str_extract(band_sds, ':[a-zA-Z0-9_]*$'))
+
+        mask_stack_reproj_file <- extension(rasterTmpFile(), '.tif')
+        mask_stack <- gdalwarp(mask_vrt_file,
                                dstfile=mask_stack_reproj_file,
                                te=out_te, t_srs=to_srs, tr=to_res, 
-                               r='near', output_Raster=TRUE, of="ENVI", 
+                               r='near', output_Raster=TRUE, of="GTiff", 
                                multi=TRUE, wo=paste0("NUM_THREADS=", n_cpus), 
                                overwrite=overwrite)
-        names(mask_stack) <- mask_bands
+        # Can't just directly assign mask_bands as the names since the bands 
+        # may have been read in different order from the HDF file
+        names(mask_stack) <- gsub('^:', '', str_extract(mask_sds, ':[a-zA-Z0-9_]*$'))
+
         if (verbose) timer <- stop_timer(timer, label=paste(image_basename, 
                                                             '-', 'cropping and reprojecting'))
 
@@ -261,7 +255,7 @@ auto_preprocess_landsat <- function(image_dirs, prefix, tc=FALSE,
 
         mask_stack_path <- file.path(this_output_path,
                                      paste(prefix, image_basename, 
-                                           'masks.envi', sep='_'))
+                                           'masks.tif', sep='_'))
         mask_stack <- writeRaster(stack(mask_stack$fill_QA,
                                         mask_stack$fmask_band),
                                   filename=mask_stack_path, 
@@ -280,7 +274,7 @@ auto_preprocess_landsat <- function(image_dirs, prefix, tc=FALSE,
             # Load dem, slope, and aspect
             slopeaspect_filename <- file.path(dem_path,
                                               paste0('slopeaspect_', 
-                                                     WRS_Path, '-', WRS_Row, '.envi'))
+                                                     WRS_Path, '-', WRS_Row, '.tif'))
             slopeaspect <- brick(slopeaspect_filename)
 
             if (!proj4comp(proj4string(image_stack), proj4string(slopeaspect))) {
@@ -312,11 +306,11 @@ auto_preprocess_landsat <- function(image_dirs, prefix, tc=FALSE,
             # used in auto_setup_dem
             slopeaspect_flt <- stack(raster(slopeaspect, layer=1) / 10000,
                                      raster(slopeaspect, layer=2) / 1000)
-            sunelev <- 90 - as.numeric(get_metadata_item(band1_imagefile, 'SolarZenith'))
-            sunazimuth <- as.numeric(get_metadata_item(band1_imagefile, 'SolarAzimuth'))
+            sunelev <- 90 - as.numeric(get_gdalinfo_item('SolarZenith', lndsr_file_gdalinfo))
+            sunazimuth <- as.numeric(get_gdalinfo_item('SolarAzimuth', lndsr_file_gdalinfo))
             output_filename <- file.path(this_output_path,
                                          paste(prefix, image_basename, 
-                                               'tc.envi', sep='_'))
+                                               'tc.tif', sep='_'))
             image_stack_tc <- topographic_corr(image_stack_masked, 
                                                slopeaspect_flt, sunelev, 
                                                sunazimuth, 
@@ -336,7 +330,7 @@ auto_preprocess_landsat <- function(image_dirs, prefix, tc=FALSE,
             # Skip topographic correction, so don't append _tc to filename
             output_filename <- file.path(this_output_path,
                                          paste0(prefix, '_', image_basename, 
-                                                '.envi'))
+                                                '.tif'))
         }
 
         image_stack <- writeRaster(image_stack, filename=output_filename, 
