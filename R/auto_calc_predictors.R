8ee84e40d7e3a064128b69c9420cf01f67a9ea8b
Alex Zvoleff <azvoleff@gmail.com>
2014-06-19 13:24:38 -0400

    Update for latest teamlucc preprocessing output.
    Support handling of alternative formats.
    Move to per-image processing - remove ability to process over image directories (too difficult to implement with varying folder structures).
    Improve docs.

--- R/auto_calc_predictors.R
+++ R/auto_calc_predictors.R
@@ -1,22 +1,61 @@
 #' Calculate predictor layers for a classification
 #'
+#' This function automates the calculation of a layer stack of predictor layers 
+#' to use in a land use and/or land cover classification. The layers in the 
+#' output layer stack are listed below. Note that all layers are rescaled so 
+#' they range between -32,767 and 32,767 so that they can be stored as 16 bit 
+#' unsigned integers.
+#'
+#' \bold{Predictor layer stack:}
+#' \tabular{lc}{
+#'     Layer 1 \tab Band 1 reflectance \cr
+#'     Layer 2 \tab Band 2 reflectance \cr
+#'     Layer 3 \tab Band 3 reflectance \cr
+#'     Layer 4 \tab Band 4 reflectance \cr
+#'     Layer 5 \tab Band 5 reflectance \cr
+#'     Layer 6 \tab Band 7 reflectance \cr
+#'     Layer 7 \tab MSAVI2 \cr
+#'     Layer 8 \tab GLCM mean (from MSAVI2) \cr
+#'     Layer 9 \tab GLCM variance (from MSAVI2) \cr
+#'     Layer 10 \tab GLCM dissimilarity (from MSAVI2) \cr
+#'     Layer 11 \tab Elevation \cr
+#'     Layer 12 \tab Slope (radians X 10000) \cr
+#'     Layer 13 \tab Aspect (see below) \cr
+#' }
+#'
+#' The aspect is recoded as:
+#'
+#' \bold{Aspect coding:}
+#' \tabular{lc}{
+#'     1 \tab north facing (0-45 degrees, 315-360 degrees) \cr
+#'     2 \tab east facing (45-135 degrees) \cr
+#'     3 \tab south facing (135-225 degrees) \cr
+#'     4 \tab west facing (225-315 degrees) \cr
+#' }
 #' @export
 #' @importFrom glcm glcm
 #' @importFrom stringr str_extract
-#' @param image_dirs list of paths to a set of Landsat images that have been 
-#' preprocessed by the \code{auto_preprocess_landsat} function.
+#' @param x path to a preprocessed image as output by 
+#' \code{auto_preprocess_landsat} or \code{auto_cloud_fill}.
+#' @param wrspath World Reference System (WRS) path
+#' @param wrsrow World Reference System (WRS) row
 #' @param dem_path path to a set of DEMs as output by \code{auto_setup_dem}
 #' @param output_path the path to use for the output (optional - if NULL then 
 #' output images will be saved alongside the input images in the same folder).
+#' @param ext file extension to use when saving output rasters (determines 
+#' output file format).
 #' @param cleartmp whether to clear temp files on each run through the loop
 #' @param overwrite whether to overwrite existing files (otherwise an error 
 #' will be raised)
 #' @param notify notifier to use (defaults to \code{print} function). See the 
 #' \code{notifyR} package for one way of sending notifications from R. The 
 #' \code{notify} function should accept a string as the only argument.
-auto_calc_predictors <- function(image_dirs, dem_path, output_path=NULL, 
-                                 cleartmp=FALSE, overwrite=FALSE,
-                                 notify=print) {
+auto_calc_predictors <- function(x, wrspath, wrsrow, dem_path, 
+                                 output_path=NULL, ext='tif', cleartmp=FALSE, 
+                                 overwrite=FALSE, notify=print) {
+    if (!file_test("-f", x)) {
+        stop(paste("input image", x, "does not exist"))
+    }
     if (!file_test("-d", dem_path)) {
         stop(paste(dem_path, "does not exist"))
     }
@@ -24,166 +63,140 @@ auto_calc_predictors <- function(image_dirs, dem_path, output_path=NULL,
         stop(paste(output_path, "does not exist"))
     }
 
+    ext <- gsub('^[.]', '', ext)
+
+    wrspath <- sprintf('%03i', wrspath)
+    wrsrow <- sprintf('%03i', wrsrow)
+
     timer <- Track_time(notify)
     timer <- start_timer(timer, label='Predictor calculation')
 
     # Setup a regex to identify preprocessed images
     preproc_regex <- '^[a-zA-Z]{2,3}_[0-9]{3}-[0-9]{3}_[0-9]{4}-[0-9]{3}_L[457][ET]SR(_tc)?'
 
-    for (image_dir in image_dirs) {
-        if (!file_test("-d", image_dir)) {
-            stop(paste(image_dir, "does not exist"))
-        }
-        preproc_files <- dir(image_dir, pattern=preproc_regex)
-        image_basenames <- unique(str_extract(preproc_files, preproc_regex))
-        if (length(image_basenames) == 0) {
-            stop(paste('no files found in', image_dir))
-        }
+    # Update the basename to refer the chosen file
+    image_basename <- basename(file_path_sans_ext(x))
 
-        for (image_basename in image_basenames) {
-            wrspathrow <- str_extract(image_basename, '_[0-9]{3}-[0-9]{3}_')
-            wrspath <- gsub('[_-]', '', str_extract(wrspathrow, '^_[0-9]{3}-'))
-            wrsrow <- gsub('[_-]', '', str_extract(wrspathrow, '-[0-9]{3}_$'))
-            timer <- start_timer(timer, label=paste0('Calculating predictors for ', wrspath, '-', wrsrow))
+    image_stack <- brick(x)
 
-            # Choose between cloud filled, gap filled, or original files, 
-            # depending on what is available
-            poss_files <- c(file.path(image_dir, paste0(image_basename, 
-                                                        '_cf_gf.tif')),
-                            file.path(image_dir, paste0(image_basename, 
-                                                        '_gf.tif')),
-                            file.path(image_dir, paste0(image_basename, 
-                                                        '_cf.tif')),
-                            file.path(image_dir, paste0(image_basename, 
-                                                        '.tif')))
-            exist_files <- unlist(lapply(poss_files, file.exists))
-            if (sum(exist_files) > 1) {
-                stop(paste('multiple file versions exist for', image_basename))
-            } else if (sum(exist_files) == 0) {
-                stop(paste('no pre-processed files found for', image_basename))
-            }
-
-            image_path <- poss_files[exist_files]
-
-            # Update the basename to refer the chosen file
-            image_basename <- basename(file_path_sans_ext(image_path))
-
-            image_stack <- brick(image_path)
-
-            if (is.null(output_path)) {
-                this_output_path <- dirname(filename(image_stack))
-            } else {
-                this_output_path <- output_path
-            }
-
-            dem_filename <- file.path(dem_path, paste0('dem_', wrspath, '-', 
-                                                       wrsrow, '.tif'))
-            image_basename_nosuffix <- gsub('_tc', '', image_basename)
-            mask_stack_file <- file.path(image_dir,
-                                         paste(image_basename_nosuffix, 'masks.tif', 
-                                               sep='_'))
-            mask_stack <- brick(mask_stack_file)
-            image_mask <- calc(mask_stack[[2]], function(maskvals) {
-                               # Mask clouds, cloud shadow, and fill
-                               (maskvals == 2) | (maskvals == 4) | (maskvals == 255)
-                               })
-
-            ######################################################################
-            # Calculate additional predictor layers (MSAVI and textures)
-            timer <- start_timer(timer, label=paste(image_basename, '-', 'MSAVI2'))
-            MSAVI2_filename <- file.path(this_output_path,
-                                         paste(image_basename, 'MSAVI2.tif', 
-                                               sep='_'))
-            MSAVI2_layer <- MSAVI2(red=raster(image_stack, layer=3),
-                                   nir=raster(image_stack, layer=4))
-            # Truncate MSAVI2 to range between 0 and 1, and scale by 10,000 so it 
-            # can be saved as a INT2S
-            MSAVI2_layer <- calc(MSAVI2_layer, fun=function(vals) {
-                    vals[vals > 1] <- 1
-                    vals[vals < 0] <- 0
-                    vals <- round(vals * 10000)
-                }, filename=MSAVI2_filename, overwrite=overwrite, datatype="INT2S")
-            timer <- stop_timer(timer, label=paste(image_basename, '-', 'MSAVI2'))
-
-            timer <- start_timer(timer, label=paste(image_basename, '-', 'glcm'))
-            MSAVI2_glcm_filename <- file.path(this_output_path,
-                                              paste(image_basename, 
-                                                    'MSAVI2_glcm.tif', 
-                                                    sep='_'))
-            glcm_statistics <- c('mean', 'variance', 'homogeneity', 'contrast', 
-                                 'dissimilarity', 'entropy', 'second_moment', 
-                                 'correlation')
-            MSAVI2_layer[image_mask] <- NA
-            # Note the min_x and max_x are given for MSAVI2 that has been 
-            # scaled by 10,000
-            MSAVI2_glcm <- apply_windowed(MSAVI2_layer, glcm, edge=c(1, 3), 
-                                          min_x=0, max_x=10000, 
-                                          filename=MSAVI2_glcm_filename, 
-                                          overwrite=overwrite, 
-                                          statistics=glcm_statistics, na_opt='center')
-            names(MSAVI2_glcm) <- paste('glcm', glcm_statistics, sep='_')
-            timer <- stop_timer(timer, label=paste(image_basename, '-', 'glcm'))
-
-            ######################################################################
-            # Load DEM, slope, and aspect, and reclass aspect
-            timer <- start_timer(timer, label=paste(image_basename, '-', 'process dem and slopeaspect'))
-
-            dem_filename <- file.path(dem_path, paste0('dem_', wrspath, '-', 
-                                                       wrsrow, '.tif'))
-            dem <- raster(dem_filename)
-
-            slopeaspect_filename <- file.path(dem_path,
-                                              paste0('slopeaspect_', wrspath, 
-                                                     '-', wrsrow, '.tif'))
-            slopeaspect <- brick(slopeaspect_filename)
-            names(slopeaspect) <- c('slope', 'aspect')
-            # Classify aspect into north facing, east facing, etc., recalling 
-            # that the aspect is stored in radians scaled by 1000.
-            #     1: north facing (0-45, 315-360)
-            #     2: east facing (45-135)
-            #     3: south facing (135-225)
-            #     4: west facing (225-315)
-            aspect_cut <- raster::cut(slopeaspect$aspect/1000,
-                                      c(-1, 45, 135, 225, 315, 361)*(pi/180))
-            # Code both 0-45 and 315-360 aspect as North facing (1)
-            aspect_cut[aspect_cut == 5] <- 1
-            names(aspect_cut) <- 'aspect'
-            timer <- stop_timer(timer, label=paste(image_basename, '-', 'process dem and slopeaspect'))
-
-            ######################################################################
-            # Layer stack predictor layers:
-            timer <- start_timer(timer, label=paste(image_basename, '-', 'write predictors'))
-            predictors <- stack(raster(image_stack, layer=1),
-                                raster(image_stack, layer=2),
-                                raster(image_stack, layer=3),
-                                raster(image_stack, layer=4),
-                                raster(image_stack, layer=5),
-                                raster(image_stack, layer=6),
-                                MSAVI2_layer,
-                                scale_raster(MSAVI2_glcm$glcm_mean),
-                                scale_raster(MSAVI2_glcm$glcm_variance),
-                                scale_raster(MSAVI2_glcm$glcm_dissimilarity),
-                                dem,
-                                slopeaspect$slope,
-                                aspect_cut)
-            predictors_filename <- file.path(this_output_path,
-                                             paste(image_basename, 
-                                                   'predictors.tif', sep='_'))
-
-            predictors <- mask(predictors, image_mask, maskvalue=1, 
-                               filename=predictors_filename, 
-                               overwrite=overwrite, datatype='INT2S')
-            names(predictors) <- c('b1', 'b2', 'b3', 'b4', 'b5', 'b7', 'msavi', 
-                                  'msavi_glcm_mean', 'msavi_glcm_variance', 
-                                  'msavi_glcm_dissimilarity', 'elev', 'slope', 
-                                  'aspect')
-            timer <- stop_timer(timer, label=paste(image_basename, '-', 'write predictors'))
-
-            timer <- stop_timer(timer, label=paste0('Calculating predictors for ', wrspath, '-', wrsrow))
-
-            if (cleartmp) removeTmpFiles(h=1)
-        }
+    if (is.null(output_path)) {
+        output_path <- dirname(x)
     }
 
+    dem_filename <- file.path(dem_path, paste0('dem_', wrspath, '-', 
+                                               wrsrow, '.', ext))
+
+    mask_stack_file <- paste0(file_path_sans_ext(x), '_masks.', ext)
+    if (!file_test('-f', mask_stack_file)) {
+        mask_stack_file <- gsub(suffix_re, paste0('_masks.', ext), img_file)
+        if (file_test('-f', mask_stack_file)) {
+            warning('using masks file with old format (pre v0.5) teamlucc naming')
+        } else {
+            stop('could not find masks file')
+        }
+    }
+    mask_stack <- brick(mask_stack_file)
+    image_mask <- calc(mask_stack[[2]], function(maskvals) {
+                       # Mask clouds, cloud shadow, and fill
+                       (maskvals == 2) | (maskvals == 4) | (maskvals == 255)
+                       })
+
+    ######################################################################
+    # Calculate additional predictor layers (MSAVI and textures)
+    timer <- start_timer(timer, label='Calculating MSAVI2')
+    MSAVI2_filename <- file.path(output_path,
+                                 paste0(image_basename, '_MSAVI2.', ext))
+    MSAVI2_layer <- MSAVI2(red=raster(image_stack, layer=3),
+                           nir=raster(image_stack, layer=4))
+    # Truncate MSAVI2 to range between 0 and 1, and scale by 10,000 so it 
+    # can be saved as a INT2S
+    MSAVI2_layer <- calc(MSAVI2_layer, fun=function(vals) {
+            vals[vals > 1] <- 1
+            vals[vals < 0] <- 0
+            vals <- round(vals * 10000)
+        }, filename=MSAVI2_filename, overwrite=overwrite, datatype="INT2S")
+    timer <- stop_timer(timer, label='Calculating MSAVI2')
+
+    timer <- start_timer(timer, label='Calculating GLCM textures')
+    MSAVI2_glcm_filename <- file.path(output_path,
+                                      paste0(image_basename, 
+                                            '_MSAVI2_glcm.', ext))
+    glcm_statistics <- c('mean', 'variance', 'homogeneity', 'contrast', 
+                         'dissimilarity', 'entropy', 'second_moment', 
+                         'correlation')
+    MSAVI2_layer[image_mask] <- NA
+    # Note the min_x and max_x are given for MSAVI2 that has been 
+    # scaled by 10,000
+    MSAVI2_glcm <- apply_windowed(MSAVI2_layer, glcm, edge=c(1, 3), 
+                                  min_x=0, max_x=10000, 
+                                  filename=MSAVI2_glcm_filename, 
+                                  overwrite=overwrite, 
+                                  statistics=glcm_statistics, na_opt='center')
+    names(MSAVI2_glcm) <- paste('glcm', glcm_statistics, sep='_')
+    timer <- stop_timer(timer, label='Calculating GLCM textures')
+
+    ######################################################################
+    # Load DEM, slope, and aspect, and reclass aspect
+    timer <- start_timer(timer, label='Processing dem and slopeaspect')
+
+    dem_filename <- file.path(dem_path, paste0('dem_', wrspath, '-', 
+                                               wrsrow, '.', ext))
+    dem <- raster(dem_filename)
+
+    slopeaspect_filename <- file.path(dem_path,
+                                      paste0('slopeaspect_', wrspath, 
+                                             '-', wrsrow, '.', ext))
+    slopeaspect <- brick(slopeaspect_filename)
+    names(slopeaspect) <- c('slope', 'aspect')
+    # Classify aspect into north facing, east facing, etc., recalling 
+    # that the aspect is stored in radians scaled by 1000.
+    #     1: north facing (0-45, 315-360)
+    #     2: east facing (45-135)
+    #     3: south facing (135-225)
+    #     4: west facing (225-315)
+    aspect_cut <- raster::cut(slopeaspect$aspect/1000,
+                              c(-1, 45, 135, 225, 315, 361)*(pi/180))
+    # Code both 0-45 and 315-360 aspect as North facing (1)
+    aspect_cut[aspect_cut == 5] <- 1
+    names(aspect_cut) <- 'aspect'
+    timer <- stop_timer(timer, label='Processing dem and slopeaspect')
+
+    ######################################################################
+    # Layer stack predictor layers:
+    timer <- start_timer(timer, label='Writing predictors')
+    predictors <- stack(raster(image_stack, layer=1),
+                        raster(image_stack, layer=2),
+                        raster(image_stack, layer=3),
+                        raster(image_stack, layer=4),
+                        raster(image_stack, layer=5),
+                        raster(image_stack, layer=6),
+                        MSAVI2_layer,
+                        scale_raster(MSAVI2_glcm$glcm_mean),
+                        scale_raster(MSAVI2_glcm$glcm_variance),
+                        scale_raster(MSAVI2_glcm$glcm_dissimilarity),
+                        dem,
+                        slopeaspect$slope,
+                        aspect_cut)
+    predictors_filename <- file.path(output_path,
+                                     paste0(image_basename, '_predictors.', 
+                                            ext))
+
+    names(predictors) <- c('b1', 'b2', 'b3', 'b4', 'b5', 'b7', 'msavi', 
+                          'msavi_glcm_mean', 'msavi_glcm_variance', 
+                          'msavi_glcm_dissimilarity', 'elev', 'slope', 
+                          'aspect')
+    predictors <- mask(predictors, image_mask, maskvalue=1, 
+                       filename=predictors_filename, 
+                       overwrite=overwrite, datatype='INT2S')
+    names(predictors) <- c('b1', 'b2', 'b3', 'b4', 'b5', 'b7', 'msavi', 
+                          'msavi_glcm_mean', 'msavi_glcm_variance', 
+                          'msavi_glcm_dissimilarity', 'elev', 'slope', 
+                          'aspect')
+    timer <- stop_timer(timer, label='Writing predictors')
+
+    if (cleartmp) removeTmpFiles(h=1)
+
     timer <- stop_timer(timer, label='Predictor calculation')
+
+    return(predictors)
 }
