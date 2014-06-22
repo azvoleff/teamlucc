6c8d9c99013507a80d813a908eecc9249b6e09dd
Alex Zvoleff <azvoleff@gmail.com>
2014-06-22 17:01:53 -0400

    Rename functions for clarity.

---
+++ R/classify.R
@@ -0,0 +1,54 @@
+#' Classifies an image using a trained random forest classifier
+#'
+#' @export
+#' @import caret randomForest
+#' @importFrom spatial.tools rasterEngine predict_rasterEngine
+#' @param x a \code{Raster*} image with the predictor layer(s) for the 
+#' classification
+#' @param model a trained random forest model as output by 
+#' \code{\link{rf_train}}
+#' @param classes_file filename for predicted classes (or missing)
+#' @param prob_file filename for predicted probabilities (or missing)
+#' @param use_training_flag indicates whether to exclude data flagged as 
+#' testing data when training the classifier. For this to work the input 
+#' train_data \code{data.frame} must have a column named 'training_flag' that 
+#' indicates, for each pixel, whether that pixel is a training pixel (coded as 
+#' TRUE) or testing pixel (coded as FALSE).
+#' @param overwrite whether to overwrite \code{out_name} if it already exists
+#' @param notify notifier to use (defaults to \code{print} function). See the 
+#' \code{notifyR} package for one way of sending notifications from R. The 
+#' \code{notify} function should accept a string as the only argument.
+#' @return a list with 2 elements: the predicted classes as a 
+#' \code{RasterLayer} and the class probabilities as a \code{RasterBrick}
+#' @examples
+#' #TODO: Fix example
+#' model <- rf_train(x, tr_data)
+#' preds <- rf_classify(x, model)
+#' require(rasterVis)
+#' levelplot(preds$classes)
+classify <- function(x, model, classes_file, prob_file, overwrite=FALSE, 
+                     notify=print) {
+    #if (missing(prob_file)) prob_file <- rasterTmpFile()
+    #TODO: find out how to use prob_file with spatial.tools
+    probs <- predict_rasterEngine(object=model, newdata=x, type='prob')
+    names(probs) <- levels(model)
+
+    # Calculate the highest probability class from the class probabilities
+    if (missing(classes_file)) classes_file <- rasterTmpFile()
+    classes <- calc(probs, fun=function(vals) {
+        # Subtract 1 below as software like ENVI starts class codes at zero
+        out <- as.numeric(which(vals == max(vals))) - 1
+        #TODO: Need to handle case of ties (length(out) > 1)
+        if (length(out) != 1) out <- NA
+        return(out)
+    }, datatype='INT2S', filename=classes_file, overwrite=overwrite)
+
+    names(classes) <- 'prediction'
+    classes <- ratify(classes)
+    rat <- levels(classes)[[1]]
+    rat$class <- levels(model)
+    rat$code <- seq(0:(nlevels(model) - 1))
+    levels(classes) <- rat
+
+    return(list(classes=classes, probs=probs))
+}
---
+++ R/train_classifier.R
@@ -0,0 +1,102 @@
+#' Trains a classifier
+#'
+#' This function trains a Support Vector Machine (SVM) or Random Forest (RF) 
+#' classifier for use in an image classification.
+#'
+#' For \code{type='SVM'}, \code{tunegrid} must be a \code{data.frame} with two 
+#' columns: ".sigma" and ".C".
+#'
+#' @export
+#' @import caret randomForest
+#' @param x a \code{Raster*} image with the predictor layer(s) for the 
+#' classification
+#' @param train_data a \code{link{pixel_data}} object
+#' @param type either "svm" (to fit a support vector machine) or "rf" (to fit a
+#' random forest).
+#' @param use_training_flag indicates whether to exclude data flagged as 
+#' testing data when training the classifier. For this to work the input 
+#' train_data \code{data.frame} must have a column named 'training_flag' that 
+#' indicates, for each pixel, whether that pixel is a training pixel (coded as 
+#' TRUE) or testing pixel (coded as FALSE).
+#' @param train_control default is NULL (reasonable values will be set 
+#' automatically).  For details see \code{\link{trainControl}}.
+#' @param tune_grid the training grid to be used for training the classifier.  
+#' See Details.
+#' @param use_rfe whether to use Recursive Feature Extraction (RFE) as 
+#' implemented in the \code{caret} package to select a subset of the input 
+#' features to be used in the classification. NOT YET SUPPORTED.
+#' @param notify notifier to use (defaults to \code{print} function). See the 
+#' \code{notifyR} package for one way of sending notifications from R. The 
+#' \code{notify} function should accept a string as the only argument.
+#' @return a trained random forest model (as a \code{train} object from the  
+#' \code{caret} package)
+train_classifier <- function(x, train_data, type='rf', use_training_flag=TRUE, 
+                             train_control=NULL, tune_grid=NULL,
+                             use_rfe=FALSE) {
+    stopifnot(type %in% c('svm', 'rf'))
+
+    if (is.null(train_control)) {
+        train_control <- trainControl(method="repeatedcv", repeats=10, 
+                                      classProbs=10)
+    }
+
+    # Build the formula, excluding the training flag column (if it exists) from 
+    # the model formula
+    predictor_names <- names(train_data@x)
+    model_formula <- formula(paste('y ~', paste(predictor_names,
+                                                collapse=' + ')))
+
+    if (use_rfe) {
+        stop('recursive feature extraction not yet supported')
+        notify('Training classifier using RFE...')
+        # This recursive feature elimination procedure follows Algorithm 19.5 
+        # in Kuhn and Johnson 2013
+        svmFuncs <- caretFuncs
+        # First center and scale
+        normalization <- preProcess(train_data@x, method='range')
+        scaled_predictors <- predict(normalization, train_data@x)
+        scaled_predictors <- as.data.frame(scaled_predictors)
+        subsets <- c(1:length(predictor_names))
+        ctrl <- rfeControl(method="repeatedcv",
+                           repeats=5,
+                           verbose=TRUE,
+                           functions=svmFuncs)
+        # For the rfe modeling, extract the training data from the main
+        # train_data dataset - no need to pass the testing data to rfe
+        rfe_x <- scaled_predictors[train_data@training_flag, ]
+        rfe_y <- train_data@y[train_data@training_flag, ]
+        rfe_res <- rfe(x=rfe_x, rfe_y,
+                       sizes=subsets,
+                       metric="ROC",
+                       rfeControl=ctrl,
+                       method="svmRadial",
+                       tuneLength=tune_length,
+                       trControl=train_control,
+                       tuneGrid=tune_grid)
+        #TODO: Extract best model from rfe_res
+    } else {
+        rfe_res <- NULL
+    }
+
+    train_data <- cbind(y=train_data@y,
+                        train_data@x,
+                        training_flag=train_data@training_flag,
+                        poly_ID=train_data@poly_ID)
+
+    if (type == 'rf') {
+        model <- train(model_formula, data=train_data, method="rf",
+                       preProc=c('range'), subset=train_data$training_flag,
+                       trControl=train_control, tuneLength=10, 
+                       tuneGrid=tune_grid, ntree=2000)
+    } else if (type == 'svm') {
+        model <- train(model_formula, data=train_data, method="svmRadial",
+                       preProc=c('range'), subset=train_data$training_flag,
+                       trControl=train_control, tuneLength=10, 
+                       tuneGrid=tune_grid)
+    } else {
+        # should never get here
+        stop("model type not recognized")
+    }
+
+    return(model)
+}
