4a86ef84f350299839171a926b4e1b175a4f5b1b
Alex Zvoleff <azvoleff@gmail.com>
2014-08-10 15:38:23 -0400

    Fix calculation of change vectors.

--- R/chg_traj.R
+++ R/chg_traj.R
@@ -1,3 +1,32 @@
+#' Calculate change-trajectory lookup table
+#'
+#' This function will format a lookup table (lut) to allow coding change 
+#' trajectories. Useful for use in conjunction with \code{\link{chg_traj}}.
+#'
+#' @export
+#' @param class_codes a list of integer codes used to code land use/cover 
+#' classes
+#' @param class_names an (optional) list of class names as character vectors
+#' @examples
+#' lut <- traj_lut(c(1, 2), c("NonForest", "Forest"))
+traj_lut <- function(class_codes, class_names=NULL) {
+    lut <- expand.grid(t0_code=class_codes, t1_code=class_codes)
+    if (!is.null(class_names)) {
+        if (length(class_names) != length(class_codes)) {
+            stop('class_names must be NULL or a vector of length equal to number of classes in initial image')
+        }
+        lut$t0_name <- class_names[match(lut$t0_code, class_codes)]
+        lut$t1_name <- class_names[match(lut$t1_code, class_codes)]
+    }
+    # Code trajectories by summing t0 and t1 after multiplying t1 by the number 
+    # of classes.
+    lut$Code <- lut$t0_code + lut$t1_code * length(class_codes)
+    # Exclude classes that are persistence - CVAPS doesn't directly code the 
+    # class for classes that persist
+    lut <- lut[!(lut$t0_code == lut$t1_code), ]
+    return(lut)
+}
+
 #' Calculate change-trajectory image
 #'
 #' This function will calculate trajectories of land cover change using the 
@@ -10,25 +39,22 @@
 #'
 #' @export
 #' @importFrom spatial.tools rasterEngine
-#' @param initial initial cover class as \code{RasterLayer}
+#' @param lut a change trajectory lookup table (LUT) as output by 
+#' \code{traj_lut}
 #' @param chg_mag change magnitude \code{RasterLayer} from \code{CVAPS}
 #' @param chg_dir change direction \code{RasterLayer} from \code{CVAPS}
 #' @param chg_threshold the threshold to use as a minimum when determining change 
 #' areas (can use \code{DFPS} to determine this value).
 #' @param filename filename to save the output \code{RasterLayer} to disk 
 #' (optional)
-#' @param classnames an optional vector of classnames to output with the 
-#' returned trajectory lookup table
 #' @param overwrite whether to overwrite existing files (otherwise an error 
 #' will be raised)
 #' @param ... additional parameters to pass to rasterEngine
-#' @return a table of all possible trajectories, with their \code{classnames} 
-#' (if specified) and the integer codes used to indicate specific trajectories 
-#' in the output image.
+#' @return a {RasterLayer} of change trajectories, coded as in \code{lut}
 #' @details Processing can be done in parallel using all using the cluster 
 #' facilities in the \code{spatial.tools} package. To enable clustering, call 
-#' \code{beginCluster} before running \code{classify}.  To stop the 
-#' cluster when finished, call \code{endCluster}.
+#' \code{beginCluster} before running \code{classify}.  To stop the cluster 
+#' when finished, call \code{endCluster}.
 #' @references Chen, J., P. Gong, C.  He, R.  Pu, and P.  Shi.  2003.
 #' Land-use/land-cover change detection using improved change-vector analysis.
 #' Photogrammetric Engineering and Remote Sensing 69:369-380.
@@ -36,55 +62,44 @@
 #' Chen, J., X. Chen, X. Cui, and J. Chen. 2011. Change vector analysis in
 #' posterior probability space: a new method for land cover change detection.
 #' IEEE Geoscience and Remote Sensing Letters 8:317-321.
-chg_traj <- function(initial, chg_mag, chg_dir, chg_threshold, filename,
-                     classnames=NULL, overwrite=FALSE, ...) {
-    if (proj4string(initial) != proj4string(chg_mag) ) {
-        stop('initial and chg_mag coordinate systems do not match')
-    } else if (proj4string(initial) != proj4string(chg_dir) ) {
-        stop('initial and chg_dir coordinate systems do not match')
-    }
-    if (extent(initial) != extent(chg_mag)) {
-        stop('extent of initial does not match extent of chg_mag')
-    } else if (extent(initial) != extent(chg_dir)) {
-        stop('extent of initial does not match extent of chg_dir')
-    }
-    if (nlayers(initial) > 1) stop('initial has more than 1 layer')
+#' @examples
+#' \dontrun{
+#' t0_train_data <- get_pixels(L5TSR_1986, L5TSR_1986_2001_training, "class_1986",training=.6)
+#' t0_model <- train_classifier(t0_train_data)
+#' t0_preds <- classify(L5TSR_1986, t0_model)
+#' t1_train_data <- get_pixels(L5TSR_2001, L5TSR_1986_2001_training, "class_2001", training=.6)
+#' t1_model <- train_classifier(t1_train_data)
+#' t1_preds <- classify(L5TSR_2001, t1_model)
+#' t0_t1_chgmag <- chg_mag(t0_preds$probs, t1_preds$probs)
+#' t0_t1_chgdir <- chg_dir(t0_preds$probs, t1_preds$probs)
+#' 
+#' lut <- traj_lut(t0_preds$codes$code, t0_preds$codes$class)
+#' t0_t1_chgtraj <- chg_traj(lut, t0_t1_chgmag, t0_t1_chgdir, .5)
+#' 
+#' # Change areas are coded following the above lookup-table (lut):
+#' plot(t0_t1_chgtraj)
+#' 
+#' # No change areas are NA:
+#' plot(is.na(t0_t1_chgtraj))
+#' }
+chg_traj <- function(lut, chg_mag, chg_dir, chg_threshold, filename, 
+                     overwrite=FALSE, ...) {
     if (nlayers(chg_mag) > 1) stop('chg_mag has more than 1 layer')
     if (nlayers(chg_dir) > 1) stop('chg_dir has more than 1 layer')
+    compareRaster(chg_mag, chg_dir)
     if (!missing(filename) && file_test('-f', filename) && !overwrite) {
         stop('output file already exists and overwrite=FALSE')
     }
 
-    # Make a lookup table of codes for each type of transition
-    classcodes <- sort(unique(getValues(initial)))
-    traj_lut <- expand.grid(t0_code=classcodes, t1_code=classcodes)
-    if (!is.null(classnames)) {
-        if (length(classnames) != length(classcodes)) {
-            stop('classnames must be NULL or a vector of length equal to number of classes in initial image')
-        }
-        traj_lut$t0_name <- classnames[match(traj_lut$t0_code, classcodes)]
-        traj_lut$t1_name <- classnames[match(traj_lut$t1_code, classcodes)]
+    calc_chg_traj <- function(chg_mag, chg_dir, chg_threshold, ...) {
+        # Trajectories in chg_dir were coded by summing t0 and t1 classes after 
+        # multiplying t1 class by the number of classes
+        chg_dir[chg_mag < chg_threshold] <- NA
+        chg_dir <- array(chg_dir, dim=c(dim(chg_mag)[1], dim(chg_mag)[2], 1))
+        return(chg_dir)
     }
-    # Code trajectories by summing t0 and t1 after multiplying t1 by the number 
-    # of classes.
-    traj_lut$Code <- traj_lut$t0_code + traj_lut$t1_code * length(classcodes)
-
-    calc_chg_traj <- function(initial, chg_mag, chg_dir, classcodes, 
-                              chg_threshold, ...) {
-        # Code change trajectories by summing t0 and chg_dir after multiplying 
-        # chg_dir by the number of classes
-        traj <- initial + chg_dir * length(classcodes)
-        # Code classes that persist, being sure to ignore NAs
-        persist_pixels <- which(chg_mag < chg_threshold)
-        persist_pixels <- persist_pixels[!is.na(persist_pixels)]
-        traj[persist_pixels] <- initial[persist_pixels] + initial[persist_pixels] * length(classcodes)
-        traj <- array(traj, dim=c(dim(initial)[1], dim(initial)[2], 1))
-        return(traj)
-    }
-    out <- rasterEngine(initial=initial, chg_mag=chg_mag, chg_dir=chg_dir, 
-                        fun=calc_chg_traj,
-                        args=list(classcodes=classcodes, 
-                                  chg_threshold=chg_threshold),
+    out <- rasterEngine(chg_mag=chg_mag, chg_dir=chg_dir, fun=calc_chg_traj,
+                        args=list(chg_threshold=chg_threshold),
                         datatype='INT2S', ...)
 
     # spatial.tools can only output the raster package grid format - so output 
@@ -95,5 +110,5 @@ chg_traj <- function(initial, chg_mag, chg_dir, chg_threshold, filename,
                            datatype='INT2S')
     }
 
-    return(list(lut=traj_lut, traj=out))
+    return(out)
 }
--- src/calc_chg_dir.cpp
+++ src/calc_chg_dir.cpp
@@ -23,12 +23,33 @@ using namespace arma;
 // [[Rcpp::export]]
 arma::ivec calc_chg_dir(arma::mat t1p, arma::mat t2p) {
     ivec chg_dir(t1p.n_rows);
-    for (int i = 0; i < t1p.n_rows; i++) {
-        // note that t1p.n_cols is equal to the number of classes
-        mat Eab = (t2p.row(i) - t1p.row(i)) * eye(t1p.n_cols, t1p.n_cols);
+    mat E = eye(t1p.n_cols, t1p.n_cols);
+    // the minus below exclues rows where Ea == Eb (persistence)
+    mat dEab(E.n_rows * E.n_rows - E.n_rows, t1p.n_cols);
+    vec traj_codes(dEab.n_rows);
+    int dEab_row = 0;
+    // Loop over time 0 (i is t0)
+    for (int i = 0; i < E.n_rows; i++) {
+        // Loop over time 1 (j is t1)
+        for (int j = 0; j < E.n_rows; j++) {
+            if (i == j) continue;
+            dEab.row(dEab_row) = E.row(j) - E.row(i);
+            // Code from=to trajectories by summing t0 and t1 codes after 
+            // multiplying t1 codes by the number of classes. Note that 
+            // t1p.n_cols is equal to the number of classes.
+            traj_codes(dEab_row) = i + j * t1p.n_cols;
+            dEab_row++;
+        }
+    }
+    for (int pix_num = 0; pix_num < t1p.n_rows; pix_num++) {
+        rowvec dot_dP_dEab(dEab.n_rows);
+        rowvec dP = t2p.row(pix_num) - t1p.row(pix_num);
+        for (int j = 0; j < dEab.n_rows; j++) {
+            dot_dP_dEab(j) = dot(dP, dEab.row(j));
+        }
         uword max_location;
-        Eab.max(max_location);
-        chg_dir(i) = max_location;
+        dot_dP_dEab.max(max_location);
+        chg_dir(pix_num) = traj_codes(max_location);
     }
     return(chg_dir);
 }
